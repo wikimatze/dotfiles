40a62291ab9dd33afc8aefd20079ae4d27f58e56
Matthias Guenther <matthias.guenther@wikimatze.de>
2012-05-12 07:16:48 +0200

    add brew_install script

---
+++ bash_scripts/brew_install.sh
@@ -0,0 +1,12 @@
+brew install ack
+brew install asciidoc
+brew install clojure
+brew install ffmpeg
+brew install htop
+brew install imagemagick
+brew install libxml2
+brew install pngcrush
+brew install streamripper
+brew install tree
+brew link libxml2
+
