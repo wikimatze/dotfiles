0c68c55bc50a77732be5d9e2796b315663ed4a21
Matthias Guenther <matthias.guenther@wikimatze.de>
2012-05-12 07:31:01 +0200

    Add rbenv-install-setup script

---
+++ bash_scripts/rbenv_install_setup.sh
@@ -0,0 +1,18 @@
+# Install rbenv {{{
+
+cd $HOME
+sudo rm -rf .rben/
+cd $HOME
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+
+# }}}
+# Install rbenv-install {{{
+
+cd $HOME/Downloads
+sudo rm -rf ruby-build
+git clone git://github.com/sstephenson/ruby-build.git
+cd ruby-build
+sudo bash install.sh
+
+# }}}
