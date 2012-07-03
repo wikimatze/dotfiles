fb77b0c161af81649c4adb3d26f499c968ca65d9
Matthias Guenther <matthias.guenther@wikimatze.de>
2012-07-03 20:48:49 +0200

    Tiny changes in ctags-install

--- bash_scripts/ctags_install.sh
+++ bash_scripts/ctags_install.sh
@@ -1,10 +1,11 @@
 cp -r $HOME/Dropbox/ctags-5.8 $HOME/Downloads
-chmod -r 777 /usr/local/bin
-chmod -r 777 /usr/local/share
+sudo chmod -R 777 /usr/local/bin
+sudo chmod -R 777 /usr/local/share
+cd $HOME/Downloads
 cd ctags-5.8
 ./configure
 make
 make install
 cd $HOME/Downloads
-rm -r ctags-5.8
+rm -rf ctags-5.8
 
