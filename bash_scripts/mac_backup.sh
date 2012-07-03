d775c8eb78a8db4fcdf2a8c427dd796d212c958c
Matthias Guenther <matthias.guenther@wikimatze.de>
2012-07-03 20:49:44 +0200

    Mac backup don't need Thunderbird folder - it's now synced with Dropbox

--- bash_scripts/mac_backup.sh
+++ bash_scripts/mac_backup.sh
@@ -11,8 +11,6 @@ mkdir $HOME/Desktop/backup/Cyberduck
 mkdir $HOME/Desktop/backup/Firefox
 mkdir $HOME/Desktop/backup/Omnigraffle
 mkdir $HOME/Desktop/backup/Skype
-mkdir $HOME/Desktop/backup/Thunderbird
-mkdir $HOME/Desktop/backup/images
 
 # }}}
 # Copy the backup file into $HOME/Desktop/backup {{{
@@ -23,7 +21,6 @@ cp -r $HOME/Library/Application\ Support/Firefox $HOME/Desktop/backup/Firefox
 cp -r $HOME/Library/Application\ Support/Google/Chrome $HOME/Desktop/backup/Chrome
 cp -r $HOME/Library/Application\ Support/Omnigraffle $HOME/Desktop/backup/Omnigraffle
 cp -r $HOME/Library/Application\ Support/Skype $HOME/Desktop/backup/Skype
-cp -r $HOME/Library/Thunderbird/ $HOME/Desktop/backup/Thunderbird
 
 # }}}
 
