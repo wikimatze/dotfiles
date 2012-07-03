b77e0e22e5e2c1e267cfbfa30a1cb3a9aa2d86e0
Matthias Guenther <matthias.guenther@wikimatze.de>
2012-07-03 21:50:19 +0200

    Add bash-script for vim installation under Linux

---
+++ bash_scripts/vim_install_linux.sh
@@ -0,0 +1,22 @@
+cd $HOME/Downloads
+git clone https://github.com/b4winckler/vim
+cd vim
+git tag -l
+git co v7-3-584
+
+./configure --prefix=/usr/local \
+  --enable-gui=no \
+  --without-x \
+  --disable-nls \
+  --with-tlib=ncurses \
+  --enable-multibyte \
+  --enable-rubyinterp \
+  --enable-pythoninterp \
+  --with-python-config-dir=$HOME/lib/python2.7/config/ \
+  --with-mac-arch=x86_64 \
+  --with-features=huge \
+  --enable-gui=gnome2
+
+sudo make
+sudo make install
+sudo make clean
