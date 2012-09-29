e6c6d3da4a81b650ce1dffac60f9a4e2d07e665b
Matthias Guenther <matthias.guenther@wikimatze.de>
2012-09-29 18:03:54 +0200

    Add backup script for Linux

---
+++ bash_scripts/ubuntu_backup.sh
@@ -0,0 +1,16 @@
+# Remove back up directory and create the new one {{{
+
+sudo rm -R $HOME/Desktop/backup
+mkdir $HOME/Desktop/backup
+
+# }}}
+# Copy the backup file into $HOME/Desktop/backup {{{
+
+cp -r $HOME/Desktop $HOME/Downloads
+mv $HOME/Downloads/Desktop $HOME/Desktop/backup
+rm -rf $HOME/Download/Desktop
+cp -r $HOME/Downloads $HOME/Desktop/backup
+cp -r $HOME/Dropbox $HOME/Desktop/backup
+
+# }}}
+
