c977410dc8ccfe1138c966ad5ff5f50246b4c2ee
Matthias Guenther <matthias.guenther@wikimatze.de>
2013-01-13 22:59:05 +0100

    Removed rules

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -330,17 +330,10 @@ awful.rules.rules = {
        buttons = clientbuttons
      }
   },
-  { rule = { class = "MPlayer" },
-    properties = { floating = true } },
-  { rule = { class = "pinentry" },
-    properties = { floating = true } },
-  { rule = { class = "gimp" },
-    properties = { floating = true } },
   -- Set Firefox to always map on tags number 2 of screen 1.
   -- { rule = { class = "Firefox" },
   --   properties = { tag = tags[1][2] } },
 }
--- }}}
 
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
