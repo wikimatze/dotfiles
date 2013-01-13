7e3ff541f3d521dc69e8f19ab18997183cb41f05
Matthias Guenther <matthias.guenther@wikimatze.de>
2013-01-13 18:18:03 +0100

    Refactored method

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -4,8 +4,9 @@ require("awful.autofocus")
 require("awful.rules")
 
 require("obvious.battery")
-require("obvious.volume_alsa")
 require("obvious.temp_info")
+require("obvious.volume_alsa")
+
 require("vicious")
 
 require("beautiful")
@@ -51,10 +52,10 @@ end
 -- {{{ Menu
 -- Create a laucher widget and a main menu
 myawesomemenu = {
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", awesome.quit }
+  { "manual", terminal .. " -e man awesome" },
+  { "edit config", editor_cmd .. " " .. awesome.conffile },
+  { "restart", awesome.restart },
+  { "quit", awesome.quit }
 }
 
 mymainmenu = awful.menu({ items =
@@ -66,17 +67,17 @@ mymainmenu = awful.menu({ items =
 
 mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
                                      menu = mymainmenu })
--- }}}
 
--- Create a textclock widget
+-- Textclock widget
 mytextclock = awful.widget.textclock({ align = "right" })
 
--- Create a systray widget
+-- Systray widget
 mysystray = widget({ type = "systray" })
 
 -- Separator widget
 separator = widget { type = "textbox" }
 separator.text = '<span color="#ee1111"> :: </span>'
+
 
 -- Create a wibox for each screen and add it
 mywibox     = {}
@@ -97,21 +98,21 @@ mytasklist.buttons = awful.util.table.join(
     if c == client.focus then
         c.minimized = true
     else
-        if not c:isvisible() then
-            awful.tag.viewonly(c:tags()[1])
-        end
-        -- This will also un-minimize
-        -- the client, if needed
-        client.focus = c
-        c:raise()
+      if not c:isvisible() then
+        awful.tag.viewonly(c:tags()[1])
+      end
+      -- This will also un-minimize
+      -- the client, if needed
+      client.focus = c
+      c:raise()
     end
   end),
   awful.button({ }, 3, function ()
     if instance then
-        instance:hide()
-        instance = nil
+      instance:hide()
+      instance = nil
     else
-        instance = awful.menu.clients({ width=250 })
+      instance = awful.menu.clients({ width=250 })
     end
   end),
   awful.button({ }, 4, function ()
@@ -147,10 +148,10 @@ for s = 1, screen.count() do
   -- Add widgets to the wibox - order matters
   mywibox[s].widgets = {
     {
-        mylauncher,
-        mytaglist[s],
-        mypromptbox[s],
-        layout = awful.widget.layout.horizontal.leftright
+      mylauncher,
+      mytaglist[s],
+      mypromptbox[s],
+      layout = awful.widget.layout.horizontal.leftright
     },
     mylayoutbox[s],
     separator,
@@ -158,9 +159,9 @@ for s = 1, screen.count() do
     separator,
     obvious.battery(),
     separator,
+    obvious.temp_info(),
     s == 1 and mysystray or nil,
     obvious.volume_alsa(),
-    obvious.temp_info(),
     mytasklist[s],
     layout = awful.widget.layout.horizontal.rightleft
   }
@@ -205,10 +206,10 @@ globalkeys = awful.util.table.join(
   awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
   awful.key({ modkey,           }, "Tab",
     function ()
-        awful.client.focus.history.previous()
-        if client.focus then
-            client.focus:raise()
-        end
+      awful.client.focus.history.previous()
+      if client.focus then
+        client.focus:raise()
+      end
     end),
 
   -- Standard program
@@ -249,110 +250,108 @@ clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
     awful.key({ modkey,           }, "n",
       function (c) -- The client currently has the input focus, so it cannot be
-          -- minimized, since minimized clients can't have the focus.
-          c.minimized = true
+        -- minimized, since minimized clients can't have the focus.
+        c.minimized = true
       end),
     awful.key({ modkey,           }, "m",
       function (c)
-          c.maximized_horizontal = not c.maximized_horizontal
-          c.maximized_vertical   = not c.maximized_vertical
+        c.maximized_horizontal = not c.maximized_horizontal
+        c.maximized_vertical   = not c.maximized_vertical
       end)
 )
 
 -- Compute the maximum number of digit we need, limited to 9
 keynumber = 0
 for s = 1, screen.count() do
-   keynumber = math.min(9, math.max(#tags[s], keynumber));
+  keynumber = math.min(9, math.max(#tags[s], keynumber));
 end
 
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it works on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
 for i = 1, keynumber do
-    globalkeys = awful.util.table.join(globalkeys,
-        awful.key({ modkey }, "#" .. i + 9,
-                  function ()
-                        local screen = mouse.screen
-                        if tags[screen][i] then
-                            awful.tag.viewonly(tags[screen][i])
-                        end
-                  end),
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
-                  function ()
-                      local screen = mouse.screen
-                      if tags[screen][i] then
-                          awful.tag.viewtoggle(tags[screen][i])
-                      end
-                  end),
-        awful.key({ modkey, "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus and tags[client.focus.screen][i] then
-                          awful.client.movetotag(tags[client.focus.screen][i])
-                      end
-                  end),
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus and tags[client.focus.screen][i] then
-                          awful.client.toggletag(tags[client.focus.screen][i])
-                      end
-                  end))
+  globalkeys = awful.util.table.join(globalkeys,
+    awful.key({ modkey }, "#" .. i + 9,
+      function ()
+        local screen = mouse.screen
+        if tags[screen][i] then
+          awful.tag.viewonly(tags[screen][i])
+        end
+      end),
+    awful.key({ modkey, "Control" }, "#" .. i + 9,
+      function ()
+        local screen = mouse.screen
+        if tags[screen][i] then
+          awful.tag.viewtoggle(tags[screen][i])
+        end
+      end),
+    awful.key({ modkey, "Shift" }, "#" .. i + 9,
+      function ()
+        if client.focus and tags[client.focus.screen][i] then
+          awful.client.movetotag(tags[client.focus.screen][i])
+        end
+      end),
+    awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
+      function ()
+        if client.focus and tags[client.focus.screen][i] then
+          awful.client.toggletag(tags[client.focus.screen][i])
+        end
+      end))
 end
 
 clientbuttons = awful.util.table.join(
-    awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
-    awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 3, awful.mouse.client.resize))
+  awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
+  awful.button({ modkey }, 1, awful.mouse.client.move),
+  awful.button({ modkey }, 3, awful.mouse.client.resize))
 
 -- Set keys
 root.keys(globalkeys)
--- }}}
 
--- {{{ Rules
+-- Rules
 awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+  -- All clients will match this rule.
+  { rule = { }, properties = { border_width = beautiful.border_width,
+    border_color = beautiful.border_normal,
+    focus = true,
+    keys = clientkeys,
+    buttons = clientbuttons } },
+  { rule = { class = "MPlayer" },
+    properties = { floating = true } },
+  { rule = { class = "pinentry" },
+    properties = { floating = true } },
+  { rule = { class = "gimp" },
+    properties = { floating = true } },
+  -- Set Firefox to always map on tags number 2 of screen 1.
+  -- { rule = { class = "Firefox" },
+  --   properties = { tag = tags[1][2] } },
 }
 -- }}}
 
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.add_signal("manage", function (c, startup)
-    -- Add a titlebar
-    -- awful.titlebar.add(c, { modkey = modkey })
+  -- Add a titlebar
+  -- awful.titlebar.add(c, { modkey = modkey })
 
-    -- Enable sloppy focus
-    c:add_signal("mouse::enter", function(c)
-        if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-            and awful.client.focus.filter(c) then
-            client.focus = c
-        end
-    end)
-
-    if not startup then
-        -- Set the windows at the slave,
-        -- i.e. put it at the end of others instead of setting it master.
-        -- awful.client.setslave(c)
-
-        -- Put windows in a smart way, only if they does not set an initial position.
-        if not c.size_hints.user_position and not c.size_hints.program_position then
-            awful.placement.no_overlap(c)
-            awful.placement.no_offscreen(c)
-        end
+  -- Enable sloppy focus
+  c:add_signal("mouse::enter", function(c)
+    if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
+        and awful.client.focus.filter(c) then
+          client.focus = c
     end
+  end)
+
+  if not startup then
+    -- Set the windows at the slave,
+    -- i.e. put it at the end of others instead of setting it master.
+    -- awful.client.setslave(c)
+
+    -- Put windows in a smart way, only if they does not set an initial position.
+    if not c.size_hints.user_position and not c.size_hints.program_position then
+      awful.placement.no_overlap(c)
+      awful.placement.no_offscreen(c)
+    end
+  end
 end)
 
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
