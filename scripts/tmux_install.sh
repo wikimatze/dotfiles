5af85d54404e7ce34c39ed1251c6fb38b0ba8d7d
Matthias Günther <matthias.guenther@wikimatze.de>
2013-05-01 06:04:35 +0200

    Update installation of tmux to version 1.8

--- scripts/tmux_install.sh
+++ scripts/tmux_install.sh
@@ -1,30 +1,25 @@
 # Create the $HOME/lib folder {{{
 
-mkdir $HOME/lib
+mkdir $HOME/Downloads
 
 # }}}
 # Get the sources {{{
 
-curl -OL https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz
-curl -OL http://downloads.sourceforge.net/tmux/tmux-1.6.tar.gz
+curl -OL http://downloads.sourceforge.net/project/levent/libevent/libevent-2.0/libevent-2.0.21-stable.tar.gz
+curl -OL http://downloads.sourceforge.net/tmux/tmux-1.8.tar.gz
 
-tar -xvzf libevent-2.0.19-stable.tar.gz
-tar -xvzf tmux-1.6.tar.gz
+tar -xvzf libevent-2.0.21-stable.tar.gz
+tar -xvzf tmux-1.8.tar.gz
 
 # }}}
 # Compiling libevent {{{
 
-cd libevent-2.0.19-stable
-./configure --prefix=/opt
-make
-sudo make install
+cd $HOME/Downloads/libevent-2.0.21-stable && ./configure --prefix=/opt && make && sudo make install
 
 # }}}
 # Compiling tmux {{{
-cd ../tmux-1.6
-LDFLAGS="-L/opt/lib" CPPFLAGS="-I/opt/include" LIBS="-lresolv" ./configure --prefix=/opt
-make
-sudo make install
+cd $HOME/Downloads/tmux-1.8
+LDFLAGS="-L/opt/lib" CPPFLAGS="-I/opt/include" LIBS="-lresolv" ./configure --prefix=/opt && make && sudo make install
 
 # }}}
 # Move the tmux-bin file in the right directory where it can be executed {{{
@@ -32,11 +27,4 @@ sudo make install
 sudo mv -f /opt/bin/tmux $HOME/bin/
 
 # }}}
-# Cleanup {{{
-
-rm -rf libevent-2.0.19*
-rm -rf tmux-1.6*
-
-# }}}
-
 
