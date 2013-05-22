a8c8970d9e0affb194e46075003ba72d883352e3
Matthias Günther <matthias.guenther@wikimatze.de>
2013-05-22 05:16:36 +0200

    Add now python specific installation 2.7 and 3.3

---
+++ scripts/python_install_2_7.sh
@@ -0,0 +1,13 @@
+cd /tmp
+wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
+tar xjvf Python-2.7.3.tar.bz2
+cd Python-2.7.3
+./configure --prefix=$HOME
+make && make install
+make inclinstall
+hash -r
+
+# cleanup
+cd /tmp
+rm -rf Python-2.7.3
+
