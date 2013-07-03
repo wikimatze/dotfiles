a15009a7041e0b919d303b3126c79b78616f6439
Matthias Günther <matthias.guenther@wikimatze.de>
2013-07-03 20:31:33 +0200

    Fixed latex packages install

--- scripts/latex_packages_install.sh
+++ scripts/latex_packages_install.sh
@@ -1,7 +1,7 @@
-sudo cp -r $HOME/Dropbox/dotfiles/latex_pakage_install/german /usr/share/texmf/tex/latex
-sudo cp -r $HOME/Dropbox/dotfiles/latex_pakage_install/picins /usr/share/texmf/tex/latex
-sudo cp -r $HOME/Dropbox/dotfiles/latex_pakage_install/stmaryrd /usr/share/texmf/tex/latex
-sudo cp -r $HOME/Dropbox/dotfiles/latex_pakage_install/yfonts /usr/share/texmf/tex/latex
+sudo cp -r $HOME/Dropbox/dotfiles/latex_package_install/german /usr/share/texmf/tex/latex
+sudo cp -r $HOME/Dropbox/dotfiles/latex_package_install/picins /usr/share/texmf/tex/latex
+sudo cp -r $HOME/Dropbox/dotfiles/latex_package_install/stmaryrd /usr/share/texmf/tex/latex
+sudo cp -r $HOME/Dropbox/dotfiles/latex_package_install/yfonts /usr/share/texmf/tex/latex
 
 sudo texhash
 
