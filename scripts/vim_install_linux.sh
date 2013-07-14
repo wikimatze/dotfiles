9d2b4c31e0f8a304cf1db826f0ebac049275e666
Matthias Günther <matthias.guenther@wikimatze.de>
2013-07-14 15:17:02 +0200

    Switched over to vim7.4 beta

--- scripts/vim_install_linux.sh
+++ scripts/vim_install_linux.sh
@@ -1,17 +1,15 @@
 cd /tmp && git clone https://github.com/b4winckler/vim && cd vim
-git co v7-3-1298
+git checkout v7-4a-013
 
 ./configure --prefix=/usr/local \
   --without-x \
   --disable-nls \
   --enable-gui=no \
-  --enable-luainterp \
   --enable-multibyte \
   --enable-python3interp \
   --enable-rubyinterp \
-  --enable-gui=gnome2
+  --enable-gui=gnome2 \
   --with-features=huge \
-  --with-mac-arch=x86_64 \
   --with-python-config-dir=$HOME/lib/config-3.3m \
   --with-tlib=ncurses \
 
