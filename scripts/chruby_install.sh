6b9cfba872a651d322793c15399dd9670e2237e6
Matthias Günther <matthias.guenther@wikimatze.de>
2013-09-30 20:06:26 +0200

    Rework chruby install script

--- scripts/chruby_install.sh
+++ scripts/chruby_install.sh
@@ -1,18 +1,18 @@
-cd /tmp wget -O ruby-install-0.3.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.3.0.tar.gz
-tar -xzvf ruby-install-0.3.0.tar.gz
-cd ruby-install-0.3.0/
-sudo make install
-
-echo "Installing ruby-install done ..."
-exec $SHELL
-echo "Installing the latest stable ruby version"
-ruby-build ruby
-
+LATEST_RUBY=2.0.0-p247
 SECOND_LATEST_RUBY=1.9.3-p429
+cd /tmp && git clone https://github.com/postmodern/ruby-install.git
+cd ruby-install && sudo make install
+echo "Done with ruby-install ..."
+
+exec $SHELL
+
+echo "Installing the latest stable ruby version"
+ruby-install ruby $LATEST_RUBY
+
 echo "Installing ruby $SECOND_LATEST_RUBY"
 ruby-install ruby $SECOND_LATEST_RUBY
 
-cd /tmp && wget -O chruby-0.3.7.tar.gz https://github.com/postmodern/chruby/archive/v0.3.7.tar.gz
-tar -xzvf chruby-0.3.7.tar.gz
-cd chruby-0.3.7/
-sudo make install
+cd /tmp && git clone https://github.com/postmodern/chruby.git
+cd chruby && sudo make install
+
+echo "Done with chruby ..."
