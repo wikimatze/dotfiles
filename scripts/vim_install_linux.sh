bb041c338acb421d18ed5a3ab568968a55182132
Matthias Günther <matthias.guenther@wikimatze.de>
2013-09-30 18:16:52 +0200

    Vim will now not be downloaded everytime when it is downloaded

--- scripts/vim_install_linux.sh
+++ scripts/vim_install_linux.sh
@@ -1,17 +1,18 @@
-cd /tmp && git clone https://github.com/b4winckler/vim && cd vim
-git checkout v7-4a-013
+cd ~/git-repositories/vim && git checkout v7-4-044
 
 ./configure --prefix=/usr/local \
   --without-x \
   --disable-nls \
   --enable-gui=no \
   --enable-multibyte \
-  --enable-python3interp \
+  --enable-pythoninterp \
+  --with-python-config-dir=$HOME/lib/python2.7/config \
   --enable-rubyinterp \
   --enable-gui=gnome2 \
   --with-features=huge \
-  --with-python-config-dir=$HOME/lib/config-3.3m \
   --with-tlib=ncurses \
 
 sudo make && sudo make install && sudo make clean
 
+#   --enable-python3interp \
+#   --with-python3-config-dir=$HOME/lib/python3.3/config-3.3m \
