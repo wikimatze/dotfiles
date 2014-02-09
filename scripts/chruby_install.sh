c2e3d5ecf63a9f95c8bdff20961e660e42f384fb
Matthias Guenther <matthias@wikimatze.de>
2014-02-09 09:17:57 +0100

    Add tmuxifier setting for padrinobook-website

--- scripts/chruby_install.sh
+++ scripts/chruby_install.sh
@@ -6,8 +6,8 @@ echo "Done with ruby-install ..."
 echo "Installing the latest stable ruby version"
 /usr/local/bin/ruby-install ruby stable
 
-echo "Installing ruby $SECOND_LATEST_RUBY"
-/usr/local/bin/ruby-install ruby $SECOND_LATEST_RUBY
+# echo "Installing ruby $SECOND_LATEST_RUBY"
+# /usr/local/bin/ruby-install ruby $SECOND_LATEST_RUBY
 
 echo "Done with installing ruby versions"
 
---
+++ tmuxifier_layouts/padrinobook-website.window.sh
@@ -0,0 +1,24 @@
+window_root "$HOME/Dropbox/padrinobook-website"
+new_window "main"
+
+run_cmd "vim"
+
+split_v 20
+run_cmd "bundle exec rake s"
+
+select_pane 1
+
+new_window "index"
+run_cmd "vim index.md"
+
+new_window "book-index"
+run_cmd "vim book_index.md"
+
+new_window "layout-default"
+run_cmd "vim _layouts/default.html"
+
+new_window "css-style"
+run_cmd "vim css/style.css"
+
+new_window "config"
+run_cmd "vim _config.yml"
