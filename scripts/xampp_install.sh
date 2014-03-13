46eb4a049b4f4ce34d59c5590938e4788f7e1576
Matthias Guenther <matthias@wikimatze.de>
2014-03-13 19:22:18 +0100

    Add xampp install script

---
+++ scripts/xampp_install.sh
@@ -0,0 +1,3 @@
+cd /tmp && wget http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-linux-1.8.3-3-installer.run
+sudo chmod 755 xampp-linux-*-installer.run && sudo ./xampp-linux-*-installer.run
+
