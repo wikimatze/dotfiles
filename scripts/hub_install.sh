0e2c97a89a6de4099610053c0ee7faa0577da345
Matthias Guenther <matthias@wikimatze.de>
2014-04-22 20:09:01 +0200

    Add tmuxifier installation script

--- scripts/hub_install.sh
+++
@@ -1,2 +0,0 @@
-cd /tmp && git clone git://github.com/github/hub.git && cd hub && rake install
-
---
+++ scripts/tmuxifier_install.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier && cd ~/.tmuxifier && git checkout v0.10.0
