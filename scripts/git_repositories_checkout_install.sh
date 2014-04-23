7f8dfdd3b1bd4f19df1d1e040c39963b274c2e5f
Matthias Guenther <matthias@wikimatze.de>
2014-04-23 06:56:33 +0200

    Add git checkout script

---
+++ scripts/git_repositories_checkout_install.sh
@@ -0,0 +1,30 @@
+DES=$HOME/git-repositoriess
+mkdir $DES
+
+cd $DES
+
+git clone https://github.com/GumbyFramework/Gumby.git
+git clone https://github.com/StudioNelstrom/vimdojo.git
+git clone https://github.com/danchoi/ri.vim.git
+git clone https://github.com/fancyapps/fancyBox.git
+git clone https://github.com/jgallen23/toc.git
+git clone https://github.com/matthias-guenther/awesome-fallout.git
+git clone https://github.com/matthias-guenther/brokenlifts-vm.git
+git clone https://github.com/matthias-guenther/hammer.vim.git
+git clone https://github.com/matthias-guenther/mailclient.git
+git clone https://github.com/matthias-guenther/padrino-livingstyleguide.git
+git clone https://github.com/matthias-guenther/padrino-zsh-completion.git
+git clone https://github.com/matthias-guenther/sweetie.git
+git clone https://github.com/matthias-guenther/tocdown.git
+git clone https://github.com/matthias-guenther/zimki.git
+git clone https://github.com/neo/ruby_koans.git
+git clone https://github.com/nightsailer/padrino-sprockets.git
+git clone https://github.com/padrino/padrino-docs.git
+git clone https://github.com/padrino/padrino-framework.git
+git clone https://github.com/padrino/padrino-recipes.git
+git clone https://github.com/padrino/padrino-web.git
+git clone https://github.com/runpaint/read-ruby.git
+git clone https://github.com/syn-systems/puppet-boilerplate-modules.git
+git clone https://github.com/vhf/free-programming-books.git
+git clone https://github.com/zurb/foundation.git
+
