901f00f2cba35b24a1334805992b2cc2283e2e5b
Matthias Guenther <matthias@wikimatze.de>
2014-05-25 06:10:30 +0200

    Sudo for some python commands

--- scripts/python_install_2_7.sh
+++ scripts/python_install_2_7.sh
@@ -3,8 +3,8 @@ wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
 tar xjvf Python-2.7.3.tar.bz2
 cd Python-2.7.3
 ./configure --prefix=$HOME
-make && make install
-make inclinstall
+sudo make && make install
+sudo make inclinstall
 hash -r
 
 # cleanup
