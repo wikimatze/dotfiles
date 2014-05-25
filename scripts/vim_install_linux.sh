bccd52501aef431c37637d622367490c8aa273a9
Matthias Guenther <matthias@wikimatze.de>
2014-05-25 06:18:11 +0200

    Vim install: remove netbeans support, take python default installation, and fail automatically if dependencies are not met

--- scripts/vim_install_linux.sh
+++ scripts/vim_install_linux.sh
@@ -1,17 +1,17 @@
-cd ~/ownCloud/vim && git checkout v7-4-258 && git clean -f
-
+cd /tmp/vim && git checkout v7-4-307 && git clean -f
+#git clone https://github.com/b4winckler/vim
 ./configure --prefix=/usr/local \
   --without-x \
   --disable-nls \
+  --disable-netbeans \
   --enable-gui=no \
   --enable-multibyte \
   --enable-rubyinterp \
   --enable-luainterp \
   --enable-pythoninterp \
-  --with-python-config-dir=$HOME/lib/python2.7/config \
-  --enable-gui=gnome2 \
   --with-features=huge \
   --with-tlib=ncurses \
+  --enable-fail-if-missing \
 
 sudo make && sudo make install && sudo make clean
 
