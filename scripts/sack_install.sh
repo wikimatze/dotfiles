32f4855c48834642cda25766795d075ac6466b13
Matthias Guenther <matthias@wikimatze.de>
2014-06-13 08:52:48 +0200

    Sack installation with go - less dependencies

--- scripts/sack_install.sh
+++ scripts/sack_install.sh
@@ -1,1 +1,3 @@
-cd /tmp && git clone https://github.com/sampson-chen/sack.git && cd sack && chmod +x install_sack.sh && ./install_sack.sh
+#!/bin/bash
+mkdir -p $HOME/bin
+curl -sSL https://github.com/zph/go-sack/raw/master/src/install | bash -s
