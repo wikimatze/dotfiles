3ab6079cbf6b7c18b110375690df8432df72b990
Matthias Guenther <matthias@wikimatze.de>
2014-06-13 12:41:51 +0200

    xampp install for 64bit

--- scripts/xampp_install.sh
+++ scripts/xampp_install.sh
@@ -1,3 +1,12 @@
-cd /tmp && wget http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-linux-1.8.3-3-installer.run
+#!/bin/bash
+if [ "$(uname -m)" == "x86_64" ]
+then
+  echo "AMD"
+  cd /tmp && wget http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-linux-x64-1.8.3-4-installer.run
+else
+  echo "Intel"
+  cd /tmp && wget http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-linux-1.8.3-4-installer.run
+fi
+
 sudo chmod 755 xampp-linux-*-installer.run && sudo ./xampp-linux-*-installer.run
 
