69d49b18e880841a7d019d442450cfebef6068bb
Matthias Guenther <matthias@wikimatze.de>
2014-06-13 10:08:23 +0200

    Add owncloud install script

---
+++ scripts/owncloud_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
+sudo apt-get update
+sudo apt-get install owncloud-client
+
