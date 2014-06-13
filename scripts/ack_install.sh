35e5771849eee2458306b64ee95fc750e826cfdf
Matthias Guenther <matthias@wikimatze.de>
2014-06-13 09:10:53 +0200

    Install ag release version

--- scripts/ack_install.sh
+++ scripts/ack_install.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
 sudo curl http://beyondgrep.com/ack-2.12-single-file > /usr/local/bin/ack && sudo chmod 0755 /usr/local/bin/ack
+
--- scripts/ag_install.sh
+++ scripts/ag_install.sh
@@ -1,1 +1,3 @@
-cd /tmp && git clone https://github.com/ggreer/the_silver_searcher ag && cd ag && ./build.sh && sudo make install
+#!/bin/bash
+cd /tmp && git clone https://github.com/ggreer/the_silver_searcher ag && cd ag && git checkout 0.22.0 && ./build.sh && sudo make install
+
