7a27e368fecae711e926714208199b37b0d139a4
Matthias Guenther <matthias@wikimatze.de>
2014-06-13 11:47:53 +0200

    Remove clutter from tmux installation

--- scripts/tmux_install.sh
+++ scripts/tmux_install.sh
@@ -1,29 +1,13 @@
-# Create the $HOME/lib folder {{{
+#!/bin/bash
+cd /tmp && curl -OL http://downloads.sourceforge.net/project/levent/libevent/libevent-2.0/libevent-2.0.21-stable.tar.gz && tar -xvzf libevent-2.0.21-stable.tar.gz && cd /tmp/libevent-2.0.21-stable && ./configure --prefix=/opt && make && sudo make install
 
-cd /tmp
-
-# }}}
-# Get the sources {{{
-
-curl -OL http://downloads.sourceforge.net/project/levent/libevent/libevent-2.0/libevent-2.0.21-stable.tar.gz
-tar -xvzf libevent-2.0.21-stable.tar.gz &&
-
-# }}}
-# Compiling libevent {{{
-
-cd /tmp/libevent-2.0.21-stable && ./configure --prefix=/opt && make && sudo make install
-
-# }}}
-# Compiling tmux {{{
+# Compiling tmux
 cd /tmp && git clone git://git.code.sf.net/p/tmux/tmux-code tmux
 cd /tmp/tmux && git checkout 1.9a
 bash autogen.sh
 LDFLAGS="-L/opt/lib" CPPFLAGS="-I/opt/include" LIBS="-lresolv" ./configure --prefix=/opt && make && sudo make install
 
-# }}}
-# Move the tmux-bin file in the right directory where it can be executed {{{
+# Move the tmux-bin file in the right directory where it can be executed
 
 sudo mv -f /opt/bin/tmux /usr/local/bin
-
-# }}}
 
