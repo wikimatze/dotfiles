3aee8a6e9ca5c160c85b4853b7ebf55076ce8410
Matthias Guenther <matthias@wikimatze.de>
2014-06-13 08:51:20 +0200

    handbrake install difference between operating systems

--- scripts/handbrake_install.sh
+++ scripts/handbrake_install.sh
@@ -1,1 +1,11 @@
-cd /tmp && wget -c https://launchpad.net/~stebbins/+archive/handbrake-releases/+files/handbrake-gtk_0.9.9ppa1~oneiric1_i386.deb && sudo dpkg -i handbrake*
+#!/bin/bash
+cd /tmp
+if [ "$(uname -m)" == "x86_64" ]
+then
+  wget https://launchpad.net/~stebbins/+archive/handbrake-releases/+files/handbrake-gtk_0.9.9ppa1~raring1_amd64.deb
+else
+  wget https://launchpad.net/~stebbins/+archive/handbrake-releases/+files/handbrake-gtk_0.9.9ppa1~raring1_i386.deb
+fi
+
+sudo dpkg -i handbrake*
+
