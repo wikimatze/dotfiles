837986486b220822a8335567e58cf51b01132421
Matthias Guenther <matthias@wikimatze.de>
2014-10-01 11:30:09 +0200

    Upade ag and tmuxifier

--- scripts/ag_install.sh
+++ scripts/ag_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/ggreer/the_silver_searcher ag && cd ag && git checkout 0.22.0 && ./build.sh && sudo make install
+cd /tmp && git clone https://github.com/ggreer/the_silver_searcher ag && cd ag && git checkout 0.24.1 && ./build.sh && sudo make install
 
--- scripts/tmuxifier_install.sh
+++ scripts/tmuxifier_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf ~/.tmuxifier
-git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier && cd ~/.tmuxifier && git checkout v0.11.4
+git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier && cd ~/.tmuxifier && git checkout v0.11.6
 
