075db049e22b0ee789b519f1167534ccf56db204
Matthias Guenther <matthias@wikimatze.de>
2014-11-12 05:37:20 +0100

    Release number for ruby-install

--- scripts/chruby_install.sh
+++ scripts/chruby_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp && rm -rf ruby-install && git clone https://github.com/postmodern/ruby-install.git
-cd ruby-install && sudo make install
+cd ruby-install && git checkout v0.5.0 && sudo make install
 echo "Done with ruby-install ..."
 
 cd /tmp && rm -rf chruby && git clone https://github.com/postmodern/chruby.git
