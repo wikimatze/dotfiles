90f1681b55026c3de1bcc5b1d2f7d42bee199177
Matthias Guenther <matthias@wikimatze.de>
2014-11-15 08:27:51 +0100

    Update vagrant and virtualbox

--- scripts/virtualbox_install.sh
+++ scripts/virtualbox_install.sh
@@ -2,9 +2,9 @@
 cd /tmp
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget http://download.virtualbox.org/virtualbox/4.3.12/virtualbox-4.3_4.3.12-93733~Ubuntu~raring_amd64.deb
+  wget http://download.virtualbox.org/virtualbox/4.3.18/virtualbox-4.3_4.3.18-96516~Ubuntu~raring_amd64.deb
 else
-  wget http://download.virtualbox.org/virtualbox/4.3.12/virtualbox-4.3_4.3.12-93733~Ubuntu~raring_i386.deb
+  wget http://download.virtualbox.org/virtualbox/4.3.18/virtualbox-4.3_4.3.18-96516~Ubuntu~raring_i386.deb
 fi
 
 sudo dpkg -i virtualbox-*
