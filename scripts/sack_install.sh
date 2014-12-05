fee0b1f650bad26a2d35d0f06d57509e270c34c3
Matthias Guenther <matthias@wikimatze.de>
2014-12-05 06:34:39 +0100

    Remove sack because ag is faster and better

--- scripts/sack_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$(uname -m)" == "x86_64" ]
-then
-  mkdir -p $HOME/bin && curl -sSL https://github.com/zph/go-sack/raw/master/src/install | bash -s
-else
-  cd /tmp && git clone https://github.com/sampson-chen/sack.git && cd sack && chmod +x install_sack.sh && ./install_sack.sh
-fi
-
