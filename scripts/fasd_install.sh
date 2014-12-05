1ad06b2ce7a2528dc9335f21d7ac42bddf5cabe8
Matthias Guenther <matthias@wikimatze.de>
2014-12-05 06:58:06 +0100

    Install fasd via git checkout

--- scripts/fasd_install.sh
+++ scripts/fasd_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /tmp && wget https://github.com/clvv/fasd/zipball/1.0.1 && unzip 1.0.1 && cd clvv-fasd-4822024 && sudo make install
+cd /tmp && git clone https://github.com/clvv/fasd && cd fasd && git checkout 1.0.1 && sudo make install
 
