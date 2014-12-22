0adcaf5f0650bfe9c7ff51948d96add6255b380c
Matthias Guenther <matthias@wikimatze.de>
2014-12-22 06:52:39 +0100

    Add xfce-terminal installation from source

---
+++ scripts/xfce4_terminal_install.sh
@@ -0,0 +1,14 @@
+sudo apt-get install -y libvte-dev libgnomeui-dev libxfce4ui-1-dev
+
+# Credits: https://forum.xfce.org/viewtopic.php?id=9297
+git clone git://git.xfce.org/apps/xfce4-terminal /tmp/xfce4-terminal && cd /tmp/xfce4-terminal
+
+./autogen.sh \
+  --prefix=/usr \
+  --sysconfdir=/etc \
+  --libexecdir=/usr/lib/xfce4 \
+  --localstatedir=/var \
+  --disable-static \
+
+sudo make install
+
