3d1d890f3ee79958f407d6a6f9cc10861fd7a4b6
Matthias Guenther <matthias@wikimatze.de>
2015-01-14 05:45:45 +0100

    Add fish package installation package

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -91,6 +91,7 @@ sudo apt-get install -y exuberant-tags
 sudo apt-get install -y ffmpeg
 sudo apt-get install -y fig2ps
 sudo apt-get install -y fig2sty
+sudo apt-get install -y fish
 sudo apt-get install -y flac
 sudo apt-get install -y flashplugin-installer
 sudo apt-get install -y ftp
