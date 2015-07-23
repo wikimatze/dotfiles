7d4eb79ef375e1c4ce5c3836ff5a8eb1f0753853
Matthias Guenther <matthias@wikimatze.de>
2015-07-23 23:06:26 +0200

    latex: Remove german package, add rechnung stys

--- scripts/latex_packages_install.sh
+++ scripts/latex_packages_install.sh
@@ -2,10 +2,10 @@
 DIR=$HOME/ownCloud/dotfiles/latex_package_install
 DEST=/usr/share/texmf/tex/latex
 
-sudo cp -r $DIR/german $DEST
 sudo cp -r $DIR/picins $DEST
 sudo cp -r $DIR/stmaryrd $DEST
 sudo cp -r $DIR/yfonts $DEST
+sudo cp -r $DIR/rechnung $DEST
 
 sudo texhash
 
