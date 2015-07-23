9bbc7bfd160feeb80a0e667460089aa263384280
Matthias Guenther <matthias@wikimatze.de>
2015-07-23 22:52:26 +0200

    tmuxifier: Rework presentation layout

--- tmuxifier_layouts/presentation.window.sh
+++ tmuxifier_layouts/presentation.window.sh
@@ -1,10 +1,11 @@
 window_root "~/bitbucket/presentations"
-new_window "md"
+new_window "slides"
 
 run_cmd "vim slides.md"
 
 split_v 20
-run_cmd "rvm use ruby-2.0.0 && rabbit -p --slides-per-page 1 -o slides.pdf slides.md"
+run_cmd "rvm use ruby-1.9.3-p551"
+run_cmd "rabbit -p --slides-per-page 1 -o slides.pdf slides.md"
 
 new_window "pdf"
 run_cmd "evince slides.pdf &"
