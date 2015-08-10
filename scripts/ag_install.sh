bc7a4f9447af040c79ece3ee106593733617fa83
Matthias Guenther <matthias@wikimatze.de>
2015-08-10 06:34:04 +0200

    ag: dump version 0.30.0

--- scripts/ag_install.sh
+++ scripts/ag_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/ggreer/the_silver_searcher ag && cd ag && git checkout 0.27.0 && ./build.sh && sudo make install
+cd /tmp && git clone https://github.com/ggreer/the_silver_searcher ag && cd ag && git checkout 0.30.0 && ./build.sh && sudo make install
 
