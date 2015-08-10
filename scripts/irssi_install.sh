9539c486e9b9170759c3354edd4ac7ab80fb6543
Matthias Guenther <matthias@wikimatze.de>
2015-08-10 07:01:42 +0200

    irssi: dump 0.8.17

--- scripts/irssi_install.sh
+++ scripts/irssi_install.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-
-IRSSI=irssi-0.8.16.tar.gz
-IRSSIDIR=irssi-0.8.16
+#!/bin/bash 
+IRSSI=irssi-0.8.17.tar.gz
+IRSSIDIR=irssi-0.8.17
 
 cd /tmp && wget http://www.irssi.org/files/$IRSSI && tar -xvf $IRSSI && cd $IRSSIDIR && ./configure && make && sudo make install
 
