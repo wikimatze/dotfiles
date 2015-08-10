a28fc9e37cc1dc7602e019c3b3512f1c49150ef8
Matthias Guenther <matthias@wikimatze.de>
2015-08-10 06:42:39 +0200

    fasd: dump latest master SHA

--- scripts/fasd_install.sh
+++ scripts/fasd_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/clvv/fasd && cd fasd && git checkout 1.0.1 && sudo make install
+cd /tmp && git clone https://github.com/clvv/fasd && cd fasd && git checkout 61ce53be996189e1c325916e45a7dc0aa89660e3 && sudo make install
 
