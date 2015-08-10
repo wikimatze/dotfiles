b274df67e037f6d7bd5d1de54a4cb54255f573ca
Matthias Guenther <matthias@wikimatze.de>
2015-08-10 06:31:31 +0200

    ack: dump 2.14

--- scripts/ack_install.sh
+++ scripts/ack_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo curl http://beyondgrep.com/ack-2.12-single-file > /usr/local/bin/ack && sudo chmod 0755 /usr/local/bin/ack
+sudo curl http://beyondgrep.com/ack-2.14-single-file > /usr/local/bin/ack && sudo chmod 0755 /usr/local/bin/ack
 
