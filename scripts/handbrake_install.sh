e33231323bea18cb36e7525ebd0e49aaa60dcd66
Matthias Guenther <matthias@wikimatze.de>
2015-08-10 06:55:56 +0200

    handbrake: dump 0.10.2

--- scripts/handbrake_install.sh
+++ scripts/handbrake_install.sh
@@ -2,9 +2,9 @@
 cd /tmp
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget https://launchpad.net/~stebbins/+archive/handbrake-releases/+files/handbrake-gtk_0.9.9ppa1~raring1_amd64.deb
+  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.2ppa1%7Etrusty1_amd64.deb
 else
-  wget https://launchpad.net/~stebbins/+archive/handbrake-releases/+files/handbrake-gtk_0.9.9ppa1~raring1_i386.deb
+  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.2ppa1%7Etrusty1_i386.deb
 fi
 
 sudo dpkg -i handbrake*
