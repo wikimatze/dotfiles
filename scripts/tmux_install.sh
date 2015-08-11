b1cfb8d0b1b2a0f20dcf9335bdf2714e42546580
Matthias Guenther <matthias@wikimatze.de>
2015-08-11 07:00:36 +0200

    tmux: dump 2.0

--- scripts/tmux_install.sh
+++ scripts/tmux_install.sh
@@ -9,8 +9,8 @@ sudo apt-get install -y xsel
 cd /tmp && curl -OL http://downloads.sourceforge.net/project/levent/libevent/libevent-2.0/libevent-2.0.21-stable.tar.gz && tar -xvzf libevent-2.0.21-stable.tar.gz && cd /tmp/libevent-2.0.21-stable && ./configure --prefix=/opt && make && sudo make install
 
 # Compiling tmux
-cd /tmp && git clone git://git.code.sf.net/p/tmux/tmux-code tmux
-cd /tmp/tmux && git checkout 1.9a
+cd /tmp && git clone https://github.com/tmux/tmux.git tmux
+cd /tmp/tmux && git checkout 2.0
 bash autogen.sh
 LDFLAGS="-L/opt/lib" CPPFLAGS="-I/opt/include" LIBS="-lresolv" ./configure --prefix=/opt && make && sudo make install
 
