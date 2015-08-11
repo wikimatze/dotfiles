c8b542429dce1090c2127430b82803d136327ffd
Matthias Guenther <matthias@wikimatze.de>
2015-08-11 22:55:32 +0200

    xfce4-terminal: update installation

--- scripts/xfce4_terminal_install.sh
+++ scripts/xfce4_terminal_install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y libvte-dev libgnomeui-dev libxfce4ui-1-dev
+sudo apt-get install -y libvte-dev libgnomeui-dev libxfce4ui-1-dev xfce4-dev-tools
 
 # Credits: https://forum.xfce.org/viewtopic.php?id=9297
 git clone git://git.xfce.org/apps/xfce4-terminal /tmp/xfce4-terminal && cd /tmp/xfce4-terminal
