3654e06e35c5a25b95b23e609a1e00dca6d9ce19
Matthias Guenther <matthias@wikimatze.de>
2015-08-13 06:30:55 +0200

    tmuxifier-layouts: add lernhelfer-admin

---
+++ tmuxifier_layouts/die-lernhelfer-admin.session.sh
@@ -0,0 +1,7 @@
+if initialize_session "die-lernhelfer-admin"; then
+  load_window "die-lernhelfer-admin"
+
+  select_window 1
+fi
+
+finalize_and_go_to_session
---
+++ tmuxifier_layouts/die-lernhelfer-admin.window.sh
@@ -0,0 +1,15 @@
+window_root "~/git-repositories/dielernhelfer"
+new_window "main"
+
+run_cmd "vim"
+
+split_v 20
+run_cmd "bundle exec padrino s"
+
+select_pane 1
+
+new_window "application.erb"
+run_cmd "vim admin/views/layouts/application.erb"
+
+new_window "lernhelfer notes"
+run_cmd "vim ~/ownCloud/notes/lernhelfer.txt"
