f73d99e630862a060c5d827a4f463d7b64073aaf
Matthias Guenther <matthias@wikimatze.de>
2015-08-13 20:49:10 +0200

    owncloud: update installation

--- scripts/owncloud_install.sh
+++ scripts/owncloud_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.10/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
 sudo apt-get update
 sudo apt-get install owncloud-client
 
