457b88d714f1b82dcb2987836c4221239f5c0f32
Matthias Guenther <matthias@wikimatze.de>
2015-08-19 06:17:01 +0200

    git: update checkout file

--- scripts/git_repositories_checkout_install.sh
+++ scripts/git_repositories_checkout_install.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
-DES=$HOME/git-repositories
+DES=$HOME/git
 SRC=https://github.com
-mkdir $DES && cd $DES
+mkdir -p $DES && cd $DES
 
-git clone $SRC/JoshCheek/ruby-kickstart.git
-git clone $SRC/GumbyFramework/Gumby.git
-git clone $SRC/StudioNelstrom/vimdojo.git
-git clone $SRC/danchoi/ri.vim.git
-git clone $SRC/fancyapps/fancyBox.git
-git clone $SRC/jgallen23/toc.git
-git clone $SRC/neo/ruby_koans.git
-git clone $SRC/nightsailer/padrino-sprockets.git
-git clone $SRC/padrino/padrino-docs.git
-git clone $SRC/padrino/padrino-framework.git
-git clone $SRC/padrino/padrino-recipes.git
-git clone $SRC/padrino/padrino-web.git
-git clone $SRC/runpaint/read-ruby.git
-git clone $SRC/syn-systems/puppet-boilerplate-modules.git
-git clone $SRC/vhf/free-programming-books.git
-git clone $SRC/wikimatze/awesome-fallout.git
-git clone $SRC/wikimatze/brokenlifts-vm.git
-git clone $SRC/wikimatze/hammer.vim.git
-git clone $SRC/wikimatze/mailclient.git
-git clone $SRC/wikimatze/mdbook.git
-git clone $SRC/wikimatze/padrino-livingstyleguide.git
-git clone $SRC/wikimatze/padrino-zsh-completion.git
-git clone $SRC/wikimatze/pumog.git
-git clone $SRC/wikimatze/sweetie.git
-git clone $SRC/wikimatze/tocdown.git
-git clone $SRC/wikimatze/zimki.git
-git clone $SRC/zurb/foundation.git
+git clone $SRC/JoshCheek/ruby-kickstart
+git clone $SRC/StudioNelstrom/vimdojo
+git clone $SRC/alexbeletsky/github-commits-widget
+git clone $SRC/danchoi/ri.vim
+git clone $SRC/fancyapps/fancyBox
+git clone $SRC/gitig/Practical-Vim-Notes
+git clone $SRC/jgallen23/toc
+git clone $SRC/livingstyleguide/livingstyleguide
+git clone $SRC/neo/ruby_koans
+git clone $SRC/nightsailer/padrino-sprockets
+git clone $SRC/nightsailer/padrino-sprockets
+git clone $SRC/padrino/padrino-docs
+git clone $SRC/padrino/padrino-framework
+git clone $SRC/padrino/padrino-recipes
+git clone $SRC/padrino/padrino-web
+git clone $SRC/runpaint/read-ruby
+git clone $SRC/sotte/presenting.vim
+git clone $SRC/sozialhelden/brokenlift
+git clone $SRC/syn-systems/puppet-boilerplate-modules
+git clone $SRC/vhf/free-programming-books
+git clone $SRC/wikimatze/awesome
+git clone $SRC/wikimatze/awesome-fallout
+git clone $SRC/wikimatze/brokenlifts-vm
+git clone $SRC/wikimatze/hammer.vim
+git clone $SRC/wikimatze/hello-world-padrino-anynines-mysql
+git clone $SRC/wikimatze/i3files
+git clone $SRC/wikimatze/job-vacancy
+git clone $SRC/wikimatze/mailclient
+git clone $SRC/wikimatze/mdbook
+git clone $SRC/wikimatze/padrino-codeship
+git clone $SRC/wikimatze/padrino-hello-world-heroku
+git clone $SRC/wikimatze/padrino-hello-world-heroku-postgres
+git clone $SRC/wikimatze/padrino-livingstyleguide
+git clone $SRC/wikimatze/padrino-observer-example-app
+git clone $SRC/wikimatze/padrino-sprockets-example
+git clone $SRC/wikimatze/padrino-vagrant
+git clone $SRC/wikimatze/padrino-zsh-completion
+git clone $SRC/wikimatze/padrinorb-website
+git clone $SRC/wikimatze/pumog
+git clone $SRC/wikimatze/sponsorpay-api
+git clone $SRC/wikimatze/surveyor
+git clone $SRC/wikimatze/sweetie
+git clone $SRC/wikimatze/tocdown
+git clone $SRC/wikimatze/tocdown
+git clone $SRC/wikimatze/vim-banshee
+git clone $SRC/wikimatze/zimki
+git clone $SRC/zurb/foundation
 
