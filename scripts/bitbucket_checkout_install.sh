f3348565efd0c11ffd7804fd5d0cb5183936f137
Matthias Guenther <matthias@wikimatze.de>
2015-08-26 20:57:30 +0200

    Update bitbucket-checkout script

--- scripts/bitbucket_checkout_install.sh
+++ scripts/bitbucket_checkout_install.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 DES=$HOME/bitbucket
-SRC=git@bitbucket.org:wikimatze
+SRC=git@bitbucket.org:
 mkdir -p $DES && cd $DES
 
-git clone $SRC/die-lernhelfer-admin
-git clone $SRC/die-lernhelfer-website
-git clone $SRC/example_book
-git clone $SRC/fragebogen-masterarbeit
-git clone $SRC/padrino-appsterdam
-git clone $SRC/padrino-spellinterieur
-git clone $SRC/pmwiki-dropcaps-recipe
-git clone $SRC/pmwiki-headlineimage-recipe
-git clone $SRC/pmwiki-linkicons-recipe
-git clone $SRC/pmwiki-syntaxlove-recipe
-git clone $SRC/pmwiki-twitter-recipe
-git clone $SRC/presentations
-git clone $SRC/rails-sample-app
-git clone $SRC/ruby-scripts
-git clone $SRC/vocabularly
+repos=(
+  wikimatze/die-lernhelfer-admin
+  wikimatze/die-lernhelfer-website
+  wikimatze/example_book
+  wikimatze/fragebogen-masterarbeit
+  wikimatze/padrino-appsterdam
+  wikimatze/padrino-spellinterieur
+  wikimatze/pmwiki-dropcaps-recipe
+  wikimatze/pmwiki-headlineimage-recipe
+  wikimatze/pmwiki-linkicons-recipe
+  wikimatze/pmwiki-syntaxlove-recipe
+  wikimatze/pmwiki-twitter-recipe
+  wikimatze/presentations
+  wikimatze/rails-sample-app
+  wikimatze/ruby-scripts
+  wikimatze/vocabularly
+)
+
+for repo in ${repos[*]}
+do
+  # cut away the repo owner
+  if [ ! -d "${repo##*/}" ]; then
+    git clone $SRC:$repo
+  fi
+done
 
