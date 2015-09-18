2ea5237ab2713cca90665be0d2cfa3a264fb6252
Matthias Guenther <matthias@wikimatze.de>
2015-09-18 07:02:00 +0200

    latex: add lineno package

--- scripts/latex_packages_install.sh
+++ scripts/latex_packages_install.sh
@@ -2,10 +2,11 @@
 DIR=$HOME/ownCloud/dotfiles/latex_package_install
 DEST=/usr/share/texmf/tex/latex
 
+sudo cp -r $DIR/lineno $DEST
 sudo cp -r $DIR/picins $DEST
+sudo cp -r $DIR/rechnung $DEST
 sudo cp -r $DIR/stmaryrd $DEST
 sudo cp -r $DIR/yfonts $DEST
-sudo cp -r $DIR/rechnung $DEST
 
 sudo texhash
 
