3a44c6f4277af2358e68e8396986d586df7fc7a5
Matthias Guenther <matthias@wikimatze.de>
2015-09-26 18:09:44 +0200

    Node installation: add v4.1.1 installation

--- scripts/node_npm_install.sh
+++ scripts/node_npm_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /tmp && git clone git://github.com/ry/node.git && cd node && ./configure && make && sudo make install
-
-sudo npm install -g csslint jshint js-yaml claymate bower phantomjs gitbook
+cd /tmp && git clone https://github.com/nodejs/node.git && cd node && git checkout v4.1.1 && ./configure && make && sudo make install
+sudo npm install -g csslint jshint js-yaml claymate bower phantomjs gitbook-cli
+rm -rf /tmp/node
 
