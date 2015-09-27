736334a7cca595b0202149828c24eb2cefe4cf0c
Matthias Guenther <matthias@wikimatze.de>
2015-09-27 18:14:31 +0200

    Add big install.sh script

---
+++ scripts/install.sh
@@ -0,0 +1,29 @@
+sudo bash ack_install.sh
+sudo bash ag_install.sh
+sudo bash bitbucket_repos_clone.sh
+sudo bash chromium_install.sh
+sudo bash ctags_install.sh
+sudo bash fasd_install.sh
+
+sudo bash firefox_flash_update.sh
+sudo bash grub_customizer.sh
+
+sudo bash handbrake_install.sh
+sudo bash heroku_install.sh
+sudo bash hub_install.sh
+sudo bash irssi_install.sh
+sudo bash latex_packages_install.sh
+bash node_npm_install.sh
+sudo bash pt_install.sh
+bash rvm_install.sh
+bash skype_install.sh
+bash tmuxifier_install.sh
+bash tmux_install.sh
+bash ttygif_install.sh
+bash vagrant_install.sh
+bash virtualbox_install.sh
+bash xfce4_terminal_install.sh
+
+
+
+
