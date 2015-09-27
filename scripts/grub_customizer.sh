54de1879a03e4d07eae9e5fc14b1b5552077db73
Matthias Guenther <matthias@wikimatze.de>
2015-09-27 17:55:04 +0200

    Grub-customizer: easier installation

--- scripts/grub_customizer.sh
+++ scripts/grub_customizer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 sudo apt-get update
-sudo apt-get install grub-customizer
+sudo apt-get install grub-customizer -y
 
