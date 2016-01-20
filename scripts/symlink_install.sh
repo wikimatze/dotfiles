9c5a046d182ee5d22139a4105e61fd57553c5da1
Matthias Guenther <matthias@wikimatze.de>
2016-01-20 22:30:59 +0100

    Remove duplication from symlinks

--- scripts/symlink_install.sh
+++ scripts/symlink_install.sh
@@ -69,10 +69,10 @@ sudo rm -f /usr/local/bin/waterproof.sh
 
 ln -sf $DIR/documents/gnupg $HOME/.gnupg
 ln -sf $DIR/documents/ssh $HOME/.ssh
-ln -sf $DIR/dotfiles/config/Terminal $HOME/.config/Terminal
-ln -sf $DIR/dotfiles/fonts $HOME/.fonts
-ln -sf $DIR/dotfiles/zsh $HOME/.zsh
-ln -sf $DIR/dotfiles/i3 $HOME/.i3
+ln -sf $DOTFILES/config/Terminal $HOME/.config/Terminal
+ln -sf $DOTFILES/fonts $HOME/.fonts
+ln -sf $DOTFILES/zsh $HOME/.zsh
+ln -sf $DOTFILES/i3 $HOME/.i3
 ln -sf $DIR/latex $HOME/latex
 
 # }}}
@@ -80,56 +80,58 @@ ln -sf $DIR/latex $HOME/latex
 
 if [ "$OSTYPE" == "linux-gnu" ]
 then
-  ln -sf $DIR/dotfiles/bashrc $HOME/.bashrc
-  ln -sf $DIR/dotfiles/functions_linux $HOME/.functions_linux
+  ln -sf $DOTFILES/bashrc $HOME/.bashrc
+  ln -sf $DOTFILES/functions_linux $HOME/.functions_linux
   sudo rm -f /etc/wpa_supplicant.conf
 elif [ "$OSTYPE" == "darwin10.0" ]
 then
-  ln -sf $DIR/dotfiles/bashrc $HOME/.bash_profile
+  ln -sf $DOTFILES/bashrc $HOME/.bash_profile
 fi
 
-ln -sf $DIR/dotfiles/convert $HOME/.convert
-ln -sf $DIR/dotfiles/functions $HOME/.functions
-ln -sf $DIR/dotfiles/latex_convert_aliases $HOME/.latex_convert_aliases
-ln -sf $DIR/dotfiles/miscellaneous $HOME/.miscellaneous
-ln -sf $DIR/dotfiles/program_aliases_linux $HOME/.program_aliases_linux
-ln -sf $DIR/dotfiles/program_aliases_mac $HOME/.program_aliases_mac
+ln -sf $DOTFILES/convert $HOME/.convert
+ln -sf $DOTFILES/functions $HOME/.functions
+ln -sf $DOTFILES/latex_convert_aliases $HOME/.latex_convert_aliases
+ln -sf $DOTFILES/miscellaneous $HOME/.miscellaneous
+ln -sf $DOTFILES/program_aliases_linux $HOME/.program_aliases_linux
+ln -sf $DOTFILES/program_aliases_mac $HOME/.program_aliases_mac
 
 # }}}
 # Miscellaneous {{{
 
-ln -sf $DIR/dotfiles/ackrc $HOME/.ackrc
-ln -sf $DIR/dotfiles/gemrc $HOME/.gemrc
-ln -sf $DIR/dotfiles/gitconfig $HOME/.gitconfig
-ln -sf $DIR/dotfiles/ptignore $HOME/.ptignore
-ln -sf $DIR/dotfiles/gitignore $HOME/.gitignore
-ln -sf $DIR/dotfiles/gitk $HOME/.gitk
-ln -sf $DIR/dotfiles/irbrc $HOME/.irbrc
-ln -sf $DIR/dotfiles/irssi $HOME/.irssi
-ln -sf $DIR/dotfiles/profile $HOME/.profile
-ln -sf $DIR/dotfiles/tigrc $HOME/.tigrc
-ln -sf $DIR/dotfiles/fish $HOME/.config/fish
-ln -sf $DIR/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -sf $DIR/dotfiles/vimperatorrc $HOME/.vimperatorrc
-ln -sf $DIR/dotfiles/zshrc $HOME/.zshrc
-ln -sf $DIR/dotfiles/Xmodmap $HOME/.Xmodmap
+ln -sf $DOTFILES/ackrc $HOME/.ackrc
+ln -sf $DOTFILES/gemrc $HOME/.gemrc
+ln -sf $DOTFILES/gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES/ptignore $HOME/.ptignore
+ln -sf $DOTFILES/gitignore $HOME/.gitignore
+ln -sf $DOTFILES/gitk $HOME/.gitk
+ln -sf $DOTFILES/irbrc $HOME/.irbrc
+ln -sf $DOTFILES/irssi $HOME/.irssi
+ln -sf $DOTFILES/profile $HOME/.profile
+ln -sf $DOTFILES/tigrc $HOME/.tigrc
+ln -sf $DOTFILES/fish $HOME/.config/fish
+ln -sf $DOTFILES/tmux.conf $HOME/.tmux.conf
+ln -sf $DOTFILES/vimperatorrc $HOME/.vimperatorrc
+ln -sf $DOTFILES/zshrc $HOME/.zshrc
+ln -sf $DOTFILES/Xmodmap $HOME/.Xmodmap
 ln -sf $DIR/zsh_history $HOME/.zsh_history
 ln -sf $DIR/fasd $HOME/.fasd
+ln -sf $DOTFILES/prezto/zpreztorc $HOME/.zpreztorc
+ln -sf $DOTFILES/prezto/prompt_wikimatze_setup $HOME/.zprezto/modules/prompt/functions/prompt_wikimatze_setup
 
 # }}}
 # Bin files {{{
 
-sudo ln -sf $DIR/dotfiles/bin/battery /usr/local/bin/battery
-sudo ln -sf $DIR/dotfiles/bin/cjpgpdf.sh /usr/local/bin/cjpgpdf.sh
-sudo ln -sf $DIR/dotfiles/bin/cpngpdf.sh /usr/local/bin/cpngpdf.sh
-sudo ln -sf $DIR/dotfiles/bin/crushjpg.sh /usr/local/bin/crushjpg.sh
-sudo ln -sf $DIR/dotfiles/bin/crushpng.sh /usr/local/bin/crushpng.sh
-sudo ln -sf $DIR/dotfiles/bin/giffnaming.sh /usr/local/bin/giffnaming.sh
-sudo ln -sf $DIR/dotfiles/bin/jnaming.sh /usr/local/bin/jnaming.sh
-sudo ln -sf $DIR/dotfiles/bin/jsmall.sh /usr/local/bin/jsmall.sh
-sudo ln -sf $DIR/dotfiles/bin/pnaming.sh /usr/local/bin/pnaming.sh
-sudo ln -sf $DIR/dotfiles/bin/psmall.sh /usr/local/bin/psmall.sh
-sudo ln -sf $DIR/dotfiles/bin/waterproof.sh /usr/local/bin/waterproof.sh
-sudo ln -sf $DIR/dotfiles/etc/default/apport /etc/default/apport
+sudo ln -sf $DOTFILES/bin/battery /usr/local/bin/battery
+sudo ln -sf $DOTFILES/bin/cjpgpdf.sh /usr/local/bin/cjpgpdf.sh
+sudo ln -sf $DOTFILES/bin/cpngpdf.sh /usr/local/bin/cpngpdf.sh
+sudo ln -sf $DOTFILES/bin/crushjpg.sh /usr/local/bin/crushjpg.sh
+sudo ln -sf $DOTFILES/bin/crushpng.sh /usr/local/bin/crushpng.sh
+sudo ln -sf $DOTFILES/bin/giffnaming.sh /usr/local/bin/giffnaming.sh
+sudo ln -sf $DOTFILES/bin/jnaming.sh /usr/local/bin/jnaming.sh
+sudo ln -sf $DOTFILES/bin/jsmall.sh /usr/local/bin/jsmall.sh
+sudo ln -sf $DOTFILES/bin/pnaming.sh /usr/local/bin/pnaming.sh
+sudo ln -sf $DOTFILES/bin/psmall.sh /usr/local/bin/psmall.sh
+sudo ln -sf $DOTFILES/bin/waterproof.sh /usr/local/bin/waterproof.sh
+sudo ln -sf $DOTFILES/etc/default/apport /etc/default/apport
 
 # }}}
