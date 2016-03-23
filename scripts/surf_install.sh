ceaf85dbd20370f74435e7ca666885f8231c6450
Matthias Guenther <matthias@wikimatze.de>
2016-03-23 21:58:51 +0100

    Update surf install script

--- scripts/surf_install.sh
+++ scripts/surf_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install libwebkitgtk-dev
+sudo apt-get install -y build-essential gnome-devel
+sudo apt-get install -y libwebkitgtk-dev
 
 cd /tmp && git clone http://git.suckless.org/surf && cd surf && sudo make && sudo make install
 rm -rf surf/
