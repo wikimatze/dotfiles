d19171428d7553156e303e03544de017e1e6178d
Matthias Guenther <matthias@wikimatze.de>
2016-05-15 07:51:43 +0200

    Add latexmk + zathura installation

---
+++ scripts/latexmk_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VERSION=445
+
+cd /tmp && rm -rf latexmk* && wget http://users.phys.psu.edu/%7Ecollins/software/latexmk-jcc/latexmk-$VERSION.zip && unzip latexmk-$VERSION.zip
+sudo cp latexmk/latexmk.pl /usr/local/bin
+sudo mv /usr/local/bin/latexmk.pl /usr/local/bin/latexmk
+
