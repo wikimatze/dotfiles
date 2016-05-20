48026d5198462c4c32fcaefc7c07fd3d07bd0947
Matthias Guenther <matthias@wikimatze.de>
2016-05-20 06:13:16 +0200

    handbrake: update 0.10.5

--- scripts/handbrake_install.sh
+++ scripts/handbrake_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd /tmp
+cd /tmp && rm -rf handbrake*
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.2ppa1%7Etrusty1_amd64.deb
+  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.5-zhb-1ppa1~trusty1_amd64.deb
 else
-  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.2ppa1%7Etrusty1_i386.deb
+  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.5-zhb-1ppa1~trusty1_i386.deb
 fi
 
 sudo dpkg -i handbrake*
