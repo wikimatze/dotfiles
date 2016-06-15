c3dbe20d5f901c37bfaab1a13997af08d1ea8a6d
Matthias Guenther <matthias@wikimatze.de>
2016-06-15 05:59:07 +0200

    zathura: compile with synctex support

--- scripts/zathura_install.sh
+++ scripts/zathura_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 GIRARA_VERSION=0.2.6
 # need this old version form vim backward/forward search
-ZATHURA_VERSION=0.3.3
+ZATHURA_VERSION=0.3.6
 
 rm -rf /tmp/girara /tmp/zathura
 
 cd /tmp && git clone https://git.pwmt.org/pwmt/girara.git && cd girara && git checkout $GIRARA_VERSION && make && sudo make install
-cd /tmp && git clone https://git.pwmt.org/pwmt/zathura.git && cd zathura && git checkout $ZATHURA_VERSION && make && sudo make install
+cd /tmp && git clone https://git.pwmt.org/pwmt/zathura.git && cd zathura && git checkout $ZATHURA_VERSION && make WITH_SYNCTEX=1 && sudo make install
 
