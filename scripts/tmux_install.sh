b606158c0a396815139954c0ab87d114fea1d8ec
Matthias Guenther <matthias@wikimatze.de>
2016-06-24 07:01:41 +0200

    tmux: switch to 2.2 and make compilation more robust (fixes #7)

--- scripts/tmux_install.sh
+++ scripts/tmux_install.sh
@@ -6,11 +6,15 @@ sudo apt-get install -y libevent-dev libncurses-dev pkg-config
 # install xsel (for copy/paste)
 sudo apt-get install -y xsel
 
+# actlocal needs those packages
+sudo apt-get install -y automake
+sudo apt-get install -y make
+
 cd /tmp && curl -OL http://downloads.sourceforge.net/project/levent/libevent/libevent-2.0/libevent-2.0.21-stable.tar.gz && tar -xvzf libevent-2.0.21-stable.tar.gz && cd /tmp/libevent-2.0.21-stable && ./configure --prefix=/opt && make && sudo make install
 
 # Compiling tmux
 cd /tmp && git clone https://github.com/tmux/tmux.git tmux
-cd /tmp/tmux && git checkout 2.1
+cd /tmp/tmux && git checkout 2.2
 bash autogen.sh
 LDFLAGS="-L/opt/lib" CPPFLAGS="-I/opt/include" LIBS="-lresolv" ./configure --prefix=/opt && make && sudo make install
 
