048bcfe318e82e0597a693479e5da79ff26a02e9
Matthias Guenther <matthias@wikimatze.de>
2016-08-28 18:15:59 +0200

    backup-script: also backups thunderbird as well as password-stores

--- scripts/ubuntu_backup.sh
+++ scripts/ubuntu_backup.sh
@@ -4,7 +4,15 @@ MONTH="`date +%b`"
 
 DEST=/media/wm/verbatim/backups/$YEAR\_$MONTH
 
-mkdir $DEST
 
-tar cvf $DEST/backup.tar.zip ~/Desktop ~/Downloads ~/ownCloud ~/Dropbox
+mkdir $DEST && cd $DEST
+
+echo "" > out.txt
+sudo nice -n 19 rsync -rltDvuWz --delete ~/Desktop "$DEST/" 2>> out.txt
+sudo nice -n 19 rsync -rltDvuWz --delete ~/ownCloud "$DEST" 2>> out.txt
+sudo nice -n 19 rsync -rltDvuWz --delete ~/Dropbox "$DEST" 2>> out.txt
+sudo nice -n 19 rsync -rltDvuWz --delete ~/Downloads "$DEST" 2>> out.txt
+sudo nice -n 19 rsync -rltDvuWz --delete ~/.thunderbird "$DEST" 2>> out.txt
+sudo nice -n 19 rsync -rltDvuWz --delete ~/.ssh "$DEST" 2>> out.txt
+sudo nice -n 19 rsync -rltDvuWz --delete ~/.password-store "$DEST" 2>> out.txt
 
