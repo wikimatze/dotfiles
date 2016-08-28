bc84cc14a8c20ac09526ec89893ddece9fc24e3a
Matthias Guenther <matthias@wikimatze.de>
2016-08-28 18:12:48 +0200

    Node 6.5.0

--- scripts/node_npm_install.sh
+++ scripts/node_npm_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/nodejs/node.git && cd node && git checkout v4.1.1 && ./configure && make && sudo make install
-sudo npm install -g csslint jshint js-yaml claymate bower phantomjs gitbook-cli
+cd /tmp && git clone https://github.com/nodejs/node.git && cd node && git checkout v6.5.0 && ./configure && make && sudo make install
+sudo npm install -g csslint jshint js-yaml claymate bower phantomjs-prebuilt gitbook-cli diff-so-fancy karma karma-cli karma-chrome-launcher karma-ng-html2js-preprocessor jasmine-core
 rm -rf /tmp/node
 
