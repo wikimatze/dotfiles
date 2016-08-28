47e76c3e7f3f0c2b6dcdc5fec927ade1580edd80
Matthias Guenther <matthias@wikimatze.de>
2016-08-28 17:09:25 +0200

    Add padrino-recipes to forked git checkouts

--- scripts/git_forked_repositories_checkout_install.sh
+++ scripts/git_forked_repositories_checkout_install.sh
@@ -3,4 +3,5 @@ DES=$HOME/git
 SRC=git@github.com:wikimatze
 
 git clone $SRC/padrino-framework.git $DES/padrino-framework-wikimatze && cd $DES/padrino-framework-wikimatze && git remote add padrino https://github.com/padrino/padrino-framework.git
+git clone $SRC/padrino-recipes.git $DES/padrino-recipes-wikimatze && cd $DES/padrino-recipes-wikimatze && git remote add padrino https://github.com/padrino/padrino-recipes.git
 
