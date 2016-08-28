4ac44b7ce14af64aa5ec3529fbeb6707bef14a40
Matthias Guenther <matthias@wikimatze.de>
2016-08-28 17:18:51 +0200

    Rvm install: install other ruby versions

--- scripts/rvm_install.sh
+++ scripts/rvm_install.sh
@@ -3,3 +3,9 @@ command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 unset GEM_HOME
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 
+rvm install ruby-1.9.3
+rvm install ruby-2.0.0
+rvm install ruby-2.1
+rvm install ruby-2.2
+rvm install ruby-2.3.1
+
