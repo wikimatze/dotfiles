00fc73499e0204a318c171e8a6decfc0003c252b
Matthias Guenther <matthias@wikimatze.de>
2016-08-29 21:33:34 +0200

    Fix zathura installation for forward/backward search packages

--- scripts/zathura_install.sh
+++ scripts/zathura_install.sh
@@ -3,6 +3,12 @@ GIRARA_VERSION=0.2.6
 # need this old version form vim backward/forward search
 ZATHURA_VERSION=0.3.6
 
+# otherwise the own girara compilation will not work
+sudo apt-get remove libgirara-dev
+
+# need for zathura compilation
+sudo apt-get install libmagic-dev
+
 rm -rf /tmp/girara /tmp/zathura
 
 cd /tmp && git clone https://git.pwmt.org/pwmt/girara.git && cd girara && git checkout $GIRARA_VERSION && make && sudo make install
