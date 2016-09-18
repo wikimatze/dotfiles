c63d702cd8aa03b2b1480b2b153f37dfeaa88179
Matthias Guenther <matthias@wikimatze.de>
2016-09-18 07:24:04 +0200

    ctags: update installation

--- scripts/ctags_install.sh
+++ scripts/ctags_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/fishman/ctags.git && cd ctags && autoreconf && ./configure && sudo make && sudo make install
+cd /tmp && git clone https://github.com/universal-ctags/ctags.git && cd ctags && ./autogen.sh && ./configure && make && sudo make install
 
