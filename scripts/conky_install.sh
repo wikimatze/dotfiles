51e08d5b3a723b4191cf20de3013acbe8f231f71
Matthias Guenther <matthias@wikimatze.de>
2016-10-08 08:05:41 +0200

    conky: fix installation

--- scripts/conky_install.sh
+++ scripts/conky_install.sh
@@ -3,4 +3,4 @@ VERSION=1.10.5
 sudo apt-get install libtolua-dev libtolua++5.1-dev libx11-dev libxft-dev libxdamage-dev libncurses5-dev libxinerama-dev cmake -y
 
 cd /tmp && git clone https://github.com/brndnmtthws/conky.git && cd conky && git checkout v$VERSION
-mkdir build && cd build && cmake .. & make & sudo make install
+mkdir build && cd build && cmake .. && make && sudo make install
