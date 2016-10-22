a48ee836b36cb6d2726579d7ca950f0c972112e9
Matthias Guenther <matthias@wikimatze.de>
2016-10-22 11:05:12 +0200

    roxterm: add default profile when changing colors

--- bin/ttl.sh
+++ bin/ttl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+dbus-send --session /net/sf/roxterm/Options net.sf.roxterm.Options.SetProfile string:$ROXTERM_ID string:'Default'
 dbus-send --session /net/sf/roxterm/Options net.sf.roxterm.Options.SetColourScheme string:$ROXTERM_ID string:'Solarized Light'
 
--- bin/ttt.sh
+++ bin/ttt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+dbus-send --session /net/sf/roxterm/Options net.sf.roxterm.Options.SetProfile string:$ROXTERM_ID string:'Default'
 dbus-send --session /net/sf/roxterm/Options net.sf.roxterm.Options.SetColourScheme string:$ROXTERM_ID string:'Tango'
 
