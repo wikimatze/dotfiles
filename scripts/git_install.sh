9a7e2226a35d4afe909be4ababd12cac006ade67
Matthias Guenther <matthias@wikimatze.de>
2016-10-27 18:07:15 +0200

    git-install: update to 2.10.1 and add the missing install step

--- scripts/git_install.sh
+++ scripts/git_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.9.3
+VERSION=2.10.1
 sudo apt-get install asciidoc xmlto docbook2x -y
 cd /tmp && wget https://github.com/git/git/archive/v$VERSION.tar.gz && tar -zxf v$VERSION.tar.gz
-cd /tmp/git-$VERSION && make configure && ./configure --prefix=/usr && make all doc info
+cd /tmp/git-$VERSION && make configure && ./configure --prefix=/usr && make all doc info && sudo make install
