0b22416ffc50c1508c2163ab5080805bbfd0de09
Matthias Guenther <matthias@wikimatze.de>
2016-10-27 22:40:45 +0200

    chromium-install: update script

--- scripts/chromium_install.sh
+++ scripts/chromium_install.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-if [ "$(uname -m)" == "x86_64" ]
-then
-  echo "AMD"
-  cd /tmp && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb
-else
-  echo "Intel"
-  cd /tmp && wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb && sudo dpkg -i google-chrome-stable_current_i386.deb
-fi
-
+sudo apt-get install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg chromium-codecs-ffmpeg-extra -y
