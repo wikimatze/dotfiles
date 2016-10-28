b0107b7a2dba0cb75d5d13ad767b5a5c34b67f01
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 10:15:44 +0200

    skype: try to install it the new way

--- scripts/skype_install.sh
+++ scripts/skype_install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-if [ "$(uname -m)" == "x86_64" ]
-then
-  cd /tmp && wget http://www.skype.com/go/getskype-linux-beta-ubuntu-64 && mv getskype-linux-beta-ubuntu-64 getskype-linux-beta-ubuntu.deb
-else
-  cd /tmp && wget http://www.skype.com/go/getskype-linux-beta-ubuntu-32 && mv getskype-linux-beta-ubuntu-32 getskype-linux-beta-ubuntu.deb
-fi
-
-sudo dpkg -i getskype-linux-beta-ubuntu.deb
-sudo apt-get -f -y install
-sudo dpkg -i getskype-linux-beta-ubuntu.deb
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install skype
 
