dadfac4d8a33514acdc4807d6875c6094d5e4e9a
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 10:29:20 +0200

    vagrant: update installation

--- scripts/vagrant_install.sh
+++ scripts/vagrant_install.sh
@@ -2,9 +2,10 @@
 cd /tmp
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb
+  wget https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6_x86_64.deb
 else
-  wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_i686.deb
+
+  wget https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6_i686.deb
 fi
 
 sudo dpkg -i vagrant*.deb
