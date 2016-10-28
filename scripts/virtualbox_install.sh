cb919222451f84e6ffb407812c53982852a98d6e
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 10:46:27 +0200

    virtualbox: update installation

--- scripts/virtualbox_install.sh
+++ scripts/virtualbox_install.sh
@@ -2,9 +2,9 @@
 cd /tmp
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget http://download.virtualbox.org/virtualbox/5.0.4/virtualbox-5.0_5.0.4-102546~Ubuntu~trusty_amd64.deb
+  wget http://download.virtualbox.org/virtualbox/5.1.8/virtualbox-5.1_5.1.8-111374~Ubuntu~yakkety_amd64.deb
 else
-  wget http://download.virtualbox.org/virtualbox/5.0.4/virtualbox-5.0_5.0.4-102546~Ubuntu~trusty_i386.deb
+  wget http://download.virtualbox.org/virtualbox/5.1.8/virtualbox-5.1_5.1.8-111374~Ubuntu~yakkety_i386.deb
 fi
 
 sudo dpkg -i virtualbox-*
