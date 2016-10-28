73d1aee75d1f38c0cb51218776ef1ff2b4365797
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 11:23:40 +0200

    vimb: add install script

---
+++ scripts/vimb_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+VERSION=2.11
+sudo apt-get install libsoup2.4-dev libwebkit-dev libgtk-3-dev libwebkitgtk-3.0-dev -y
+
+cd /tmp && git clone https://github.com/fanglingsu/vimb.git && cd vimb && git checkout $VERSION
+
+make && sudo make install && make clean
+
