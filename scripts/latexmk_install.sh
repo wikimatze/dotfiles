cb9f77d65c6c109d05ba9fa4f7c868fc61215099
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 09:24:30 +0200

    latexmk: version 4.48

--- scripts/latexmk_install.sh
+++ scripts/latexmk_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=445
+VERSION=448
 
 cd /tmp && rm -rf latexmk* && wget http://users.phys.psu.edu/%7Ecollins/software/latexmk-jcc/latexmk-$VERSION.zip && unzip latexmk-$VERSION.zip
 sudo cp latexmk/latexmk.pl /usr/local/bin
