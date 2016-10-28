c0a1bb4fc3f1611e6d84ca3f4cdf5e284370c747
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 10:20:13 +0200

    Remove surf-install script

--- scripts/surf_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install -y build-essential gnome-devel
-sudo apt-get install -y libwebkitgtk-dev
-
-cd /tmp && git clone http://git.suckless.org/surf && cd surf && sudo make && sudo make install
-rm -rf surf/
-
