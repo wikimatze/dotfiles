c01b3161adcdeb8eb00a6c113a8ac30c90061be9
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 14:59:32 +0200

    zathura-install: update script

--- scripts/zathura_install.sh
+++ scripts/zathura_install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 GIRARA_VERSION=0.2.6
-# need this old version form vim backward/forward search
+POPPLER_VERSION=0.2.6
 ZATHURA_VERSION=0.3.6
 
 # otherwise the own girara compilation will not work
 sudo apt-get remove libgirara-dev
 
+# need for girara compilation
+sudo apt-get install libgtk-3-dev
+
 # need for zathura compilation
 sudo apt-get install libmagic-dev
 
-rm -rf /tmp/girara /tmp/zathura
+rm -rf /tmp/girara /tmp/zathura /tmp/zathura-pdf-poppler
 
-cd /tmp && git clone https://git.pwmt.org/pwmt/girara.git && cd girara && git checkout $GIRARA_VERSION && make && sudo make install
-cd /tmp && git clone https://git.pwmt.org/pwmt/zathura.git && cd zathura && git checkout $ZATHURA_VERSION && make WITH_SYNCTEX=1 && sudo make install
+
+cd /tmp && git clone https://github.com/pwmt/girara.git && cd girara && git checkout $GIRARA_VERSION && make && sudo make install
+cd /tmp && git clone  https://github.com/pwmt/zathura-pdf-poppler.git && cd zathura-pdf-poppler && git checkout $POPPLER_VERSION && make & sude make install
+cd /tmp && git clone https://github.com/pwmt/zathura.git && cd zathura && git checkout $ZATHURA_VERSION && make WITH_SYNCTEX=1 && sudo make install
 
