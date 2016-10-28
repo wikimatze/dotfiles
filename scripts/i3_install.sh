2126d6a999a0becdb3887bbc6f5ecabd14dccf54
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 08:47:26 +0200

    i3: add autoconfirm for new packages

--- scripts/i3_install.sh
+++ scripts/i3_install.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-git clone https://github.com/i3/i3.git /tmp/i3 && cd /tmp/i3 && git checkout 4.11
+VERSION=4.11
+git clone https://github.com/i3/i3.git /tmp/i3 && cd /tmp/i3 && git checkout $VERSION
 
-sudo apt-get install libxcb1-dev
-sudo apt-get install libxcb-keysyms1-dev
-sudo apt-get install libxcb-util0-dev
-sudo apt-get install libxcb-icccm4-dev
-sudo apt-get install libyajl-dev
-sudo apt-get install libstartup-notification0-dev
-sudo apt-get install libxcb-randr0-dev
-sudo apt-get install libev-dev
-sudo apt-get install libxcb-xinerama0-dev
-sudo apt-get install libpango1.0-dev
-sudo apt-get install libxcursor-dev
-sudo apt-get install libxcb-cursor-dev
-sudo apt-get install libxcb-xkb-dev
-sudo apt-get install libxkbcommon-dev
-sudo apt-get install libxkbcommon-x11-dev
+sudo apt-get install -y libxcb1-dev
+sudo apt-get install -y libxcb-keysyms1-dev
+sudo apt-get install -y libxcb-util0-dev
+sudo apt-get install -y libxcb-icccm4-dev
+sudo apt-get install -y libyajl-dev
+sudo apt-get install -y libstartup-notification0-dev
+sudo apt-get install -y libxcb-randr0-dev
+sudo apt-get install -y libev-dev
+sudo apt-get install -y libxcb-xinerama0-dev
+sudo apt-get install -y libpango1.0-dev
+sudo apt-get install -y libxcursor-dev
+sudo apt-get install -y libxcb-cursor-dev
+sudo apt-get install -y libxcb-xkb-dev
+sudo apt-get install -y libxkbcommon-dev
+sudo apt-get install -y libxkbcommon-x11-dev
 
 make && sudo make install
 
