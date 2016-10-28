c57a76ef1c667a090d6ff61aec3d24c81af553fe
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 08:42:22 +0200

    handbrake: update to latest ubuntu release

--- scripts/handbrake_install.sh
+++ scripts/handbrake_install.sh
@@ -2,9 +2,9 @@
 cd /tmp && rm -rf handbrake*
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.5-zhb-1ppa1~trusty1_amd64.deb
+  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.5-zhb-1ppa1~xenial1_amd64.deb
 else
-  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.5-zhb-1ppa1~trusty1_i386.deb
+  wget https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases/+files/handbrake-gtk_0.10.5-zhb-1ppa1~xenial1_i386.deb
 fi
 
 sudo dpkg -i handbrake*
