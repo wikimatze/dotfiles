1513429f06a5f78995ac7884f3725f7177502534
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 09:21:15 +0200

    irssi: switch to version 0.8.20

--- scripts/irssi_install.sh
+++ scripts/irssi_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=0.8.19
+VERSION=0.8.20
 DIR=irssi-$VERSION
 
 cd /tmp && wget https://github.com/irssi/irssi/releases/download/$VERSION/$DIR.tar.gz && tar xf $DIR.tar.gz && cd $DIR && ./configure && make && sudo make install
