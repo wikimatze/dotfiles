23464a4ad016fe545ba94290002d20f5be54a3cc
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 17:19:38 +0200

    Add install script for simplescreenrecorder

---
+++ scripts/simplescreenrecorder_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install -y libavcodec-extra
+sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder -y
+sudo apt-get update
+sudo apt-get install simplescreenrecorder
+
