3444e111719ebc5451223ee98eef552ae8b2a7b4
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 09:33:33 +0200

    libsync: update package to 2016 version (need for vimtex)

--- scripts/libsynctex_install.sh
+++ scripts/libsynctex_install.sh
@@ -3,11 +3,11 @@ cd /tmp && rm -rf libsynctex*
 
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex1_2015.20160222.37495-1_amd64.deb
-  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex-dev_2015.20160222.37495-1_amd64.deb
+  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex1_2016.20160513.41080-6_amd64.deb
+  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex-dev_2016.20160513.41080-6_amd64.deb
 else
-  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex1_2015.20160222.37495-1_i386.deb
-  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex-dev_2015.20160222.37495-1_i386.deb
+  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex1_2016.20160513.41080-6_i386.deb
+  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex-dev_2016.20160513.41080-6_i386.deb
 fi
 
 sudo dpkg -i libsynctex1* && sudo dpkg -i libsynctex-dev*
