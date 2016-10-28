51bfb69b1df493eeb6b5d35ec283aa3274c6bba2
Matthias Guenther <matthias@wikimatze.de>
2016-10-28 07:30:02 +0200

    chromium: fix installation

--- scripts/chromium_install.sh
+++ scripts/chromium_install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg chromium-codecs-ffmpeg-extra -y
+sudo apt-get install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg -y
