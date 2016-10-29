8a488e9184e2d92ba0c4d80887a213443a8310b5
Matthias Guenther <matthias@wikimatze.de>
2016-10-29 18:32:21 +0200

    owncloud: add missing key

--- scripts/owncloud_install.sh
+++ scripts/owncloud_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
+wget http://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_16.04/Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update
+
+udo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/ /' > /etc/apt/sources.list.d/owncloud-client.list"
 sudo apt-get update
 sudo apt-get install -y owncloud-client
 
