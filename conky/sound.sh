79abc7e8a678526570a71814d56e0733c3a62f7a
Matthias Guenther <matthias@wikimatze.de>
2016-10-29 10:08:06 +0200

    i3+conky: using amixer instead of pactl to manager sound

--- conky/sound.sh
+++ conky/sound.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ `pactl list | grep "Sink #0" -A 9 | grep Mute: | grep -c yes` -eq 1 ]; then
+if [ `amixer| grep "'Master',0" -A 4 | grep -c "\[off\]"` -eq 1 ]; then
   VERSION='𝄪'
 else
-  NUMBER="`pactl list | grep "Sink #0" -A 9| grep -o -m 1 -P '(\d{1,2}%)' | head -1`"
+  NUMBER="`amixer | grep "'Master',0" -A 4 | grep -o -m 1 -P '(\d{1,3}%)'`"
   VERSION='♬'
 fi
 
