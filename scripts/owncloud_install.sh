ed46f88389dba2c3f4eb51184f32c066ec6ebb78
Matthias Guenther <matthias@wikimatze.de>
2016-10-29 16:37:08 +0200

    owncloud: switching back to 16.04

--- scripts/owncloud_install.sh
+++ scripts/owncloud_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_16.10/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
 sudo apt-get update
 sudo apt-get install owncloud-client -y
 
