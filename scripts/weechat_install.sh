14877501fd085ff0fcc3c5881dc2bbf0739c72d3
Matthias Guenther <matthias@wikimatze.de>
2016-11-05 06:45:45 +0100

    weechat: dump version 1.6

--- scripts/weechat_install.sh
+++ scripts/weechat_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.5
+VERSION=1.6
 
 cd /tmp && git clone https://github.com/weechat/weechat.git && cd weechat && git checkout v$VERSION && mkdir build && cd build && cmake .. && make && sudo make install
 
