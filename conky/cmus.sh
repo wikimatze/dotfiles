add8a58f885197f6dbc214eccf83c8cc1999fc95
Matthias Guenther <matthias@wikimatze.de>
2016-12-04 12:12:33 +0100

    conky: add cmus script

---
+++ conky/cmus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -x /usr/bin/cmus-remote ]; then
+  exit
+fi
+
+Cmus_remote=$(cmus-remote -Q)
+Instance=$(echo -e "$Cmus_remote" | wc -l)
+
+if [ $Instance -gt 1 ]; then
+  TITLE=`cmus-remote -Q | grep " title " | cut -c 11-`
+  ALBUM=`cmus-remote -Q | grep " album " | cut -c 11-`
+  ARTIST=`cmus-remote -Q | grep " albumartist " | cut -c 17-`
+  echo "$TITLE" - "$ALBUM" - "$ARTIST"
+fi
+
