92f2bcf1868f437fb9a12aae51483a51e3f52c5b
Matthias Guenther <matthias@wikimatze.de>
2016-12-05 22:29:38 +0100

    cmus-conky: display streaming info if it's on

--- conky/cmus.sh
+++ conky/cmus.sh
@@ -6,10 +6,17 @@ fi
 
 Cmus_remote=$(cmus-remote -Q)
 Instance=$(echo -e "$Cmus_remote" | wc -l)
+STREAM=`cmus-remote -Q | grep -c "stream "`
+
 
 if [ $Instance -gt 1 ]; then
-  TITLE=`cmus-remote -Q | grep " title " | cut -c 11-`
-  ARTIST=`cmus-remote -Q | grep " albumartist " | cut -c 17-`
-  echo "$TITLE" - "$ARTIST"
+  if [ $STREAM -eq 1 ]; then
+    STREAM=`cmus-remote -Q | grep "stream " | cut -c 8-`
+    echo $STREAM
+  else
+    TITLE=`cmus-remote -Q | grep " title " | cut -c 11-`
+    ARTIST=`cmus-remote -Q | grep " albumartist " | cut -c 17-`
+    echo "$TITLE" - "$ARTIST"
+  fi
 fi
 
