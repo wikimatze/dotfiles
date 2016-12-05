8b34e50f26960b3abc29438bd79ff85d48981359
Matthias Guenther <matthias@wikimatze.de>
2016-12-05 22:32:31 +0100

    weechat: improve installation script

--- scripts/weechat_install.sh
+++ scripts/weechat_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VERSION=1.6
 
-cd /tmp && git clone https://github.com/weechat/weechat.git && cd weechat && git checkout v$VERSION && mkdir build && cd build && cmake .. && make && sudo make install
+cd /tmp && rm -rf weechat && git clone https://github.com/weechat/weechat.git && cd weechat && git checkout v$VERSION && mkdir build && cd build && cmake .. && make && sudo make install
 
