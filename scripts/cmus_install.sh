235c0d46c50ba90538409124e5b286411b38614f
Matthias Guenther <matthias@wikimatze.de>
2017-01-06 19:29:22 +0100

    Add cmus install script

---
+++ scripts/cmus_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VERSION=2.7.1
+sudo apt-get build-dep cmus
+
+# get version from https://cmus.github.io/#download
+cd /tmp && wget https://github.com/cmus/cmus/archive/v$VERSION.tar.gz && tar -xf v$VERSION.tar.gz && cd cmus-$VERSION && ./configure && make && sudo make install
+
