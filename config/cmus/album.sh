19fb20d71cc006ef386f4948ab095bbe825ecf35
Matthias Guenther <matthias@wikimatze.de>
2017-01-10 18:21:34 +0100

    Remove echo from album-cmus script

--- config/cmus/album.sh
+++ config/cmus/album.sh
@@ -1,6 +1,4 @@
 ALBUM_PATH=/$(cmus-remote -Q | grep file | cut -d'/' --complement -s -f1 | rev | cut -d'/' --complement -s -f1 | rev | sed -e 's/\s/\\ /g' | sed -e 's/(/\\(/g' | sed -e 's/)/\\)/g')/cover.jpg
 ALBUM_NAME=$(cmus-remote -Q | grep -w 'tag album' | cut -d'~' --complement -f1 | cut -d' ' --complement -s -f1 | sed -e 's/\s/-/g')
-echo "$ALBUM_PATH"
-echo "$ALBUM_NAME"
 
 eval notify-send -i $ALBUM_PATH $ALBUM_NAME
