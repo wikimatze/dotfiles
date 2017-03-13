28052e1d0a516e8aa0c0812b06ac5bdcd2bc1a9a
Matthias Guenther <matthias@wikimatze.de>
2017-03-13 21:29:34 +0100

    conky: dump 1.10.6 version

--- scripts/conky_install.sh
+++ scripts/conky_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.10.5
+VERSION=1.10.6
 sudo apt-get install libtolua-dev libtolua++5.1-dev libx11-dev libxft-dev libxdamage-dev libncurses5-dev libxinerama-dev cmake -y
 
 cd /tmp && git clone https://github.com/brndnmtthws/conky.git && cd conky && git checkout v$VERSION
