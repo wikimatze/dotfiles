a86881b2d992fe7758ca91b5129b7aaee4b21083
Matthias Guenther <matthias@wikimatze.de>
2017-03-13 21:49:17 +0100

    fasd: dump new version

--- scripts/fasd_install.sh
+++ scripts/fasd_install.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/clvv/fasd && cd fasd && git checkout 61ce53be996189e1c325916e45a7dc0aa89660e3 && sudo make install
-
+cd /tmp && git clone https://github.com/clvv/fasd && cd fasd && git checkout 90b531a5daaa545c74c7d98974b54cbdb92659fc && sudo make install
