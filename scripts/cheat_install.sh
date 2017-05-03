d106ed392f05a5dd0fddd57fd2f25eb1472d8c6f
Matthias Guenther <matthias@wikimatze.de>
2017-05-03 21:46:28 +0200

    Move pip cheat install into pip-install script

--- scripts/cheat_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# See the funny https://github.com/chrisallenlane/cheat
-sudo pip install cheat
-
--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo pip install docutils # need for previm
 
+# See the funny https://github.com/chrisallenlane/cheat
+sudo pip install cheat
+
