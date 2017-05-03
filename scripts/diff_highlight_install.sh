1fe8475b02583fbb09811cb164c2861e85d38bb3
Matthias Guenther <matthias@wikimatze.de>
2017-05-03 22:05:02 +0200

    Remove diff_highlight_install.sh

--- scripts/diff_highlight_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /tmp && git clone https://github.com/git/git.git && sudo cp git/contrib/diff-highlight/diff-highlight /usr/local/bin &&  sudo chmod +x /usr/local/bin/diff-highlight
-
