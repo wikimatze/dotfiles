a403ee6170874a924bf8415c05eb162dddb7658a
Matthias Guenther <matthias@wikimatze.de>
2017-05-03 21:39:09 +0200

    Update ack-install: version 2.18, skip curl install if it's installed; extract curl into ~/bin folder (no root required anymore)

--- scripts/ack_install.sh
+++ scripts/ack_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-sudo apt-get install -y curl
-sudo curl http://beyondgrep.com/ack-2.14-single-file > /usr/local/bin/ack && sudo chmod 0755 /usr/local/bin/ack
+which curl > /dev/null 2>&1
+if [ $? = 1 ]; then
+  echo "Curl is not installed"
+  sudo apt-get install -y curl
+fi
+
+curl https://beyondgrep.com/ack-2.18-single-file > ~/bin/ack && sudo chmod 0755 ~/bin/ack
 
