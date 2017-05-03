84298964a80a7dbd9ef0819ed3d60c8006791380
Matthias Guenther <matthias@wikimatze.de>
2017-05-03 22:36:32 +0200

    Versioning of diff-so-fancy + easier installation with the needed diff-highlight dependency

--- scripts/diff_so_fancy_install.sh
+++ scripts/diff_so_fancy_install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/so-fancy/diff-so-fancy.git && sudo cp -R diff-so-fancy/libexec/ /usr/local/bin && sudo cp diff-so-fancy/diff-so-fancy /usr/local/bin && sudo chmod +x /usr/local/bin/diff-so-fancy
+DIFF_HIGHLIGHT_VERSION=d2bbb7c
+cd /tmp && git clone https://github.com/git/git.git && cd git && git checkout $DIFF_HIGHLIGHT_VERSION
+sudo cp contrib/diff-highlight/diff-highlight ~/bin && sudo chmod +x ~/bin/diff-highlight
+
+
+DIFF_SO_FANCY_VERSION=3adf011
+cd /tmp && git clone https://github.com/so-fancy/diff-so-fancy.git && cd diff-so-fancy && git checkout $DIFF_SO_FANCY_VERSION
+sudo cp diff-so-fancy ~/bin && sudo chmod +x ~/bin/diff-so-fancy
 
