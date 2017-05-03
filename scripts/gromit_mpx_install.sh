87e1842251551eebc02ff36327038b227e49aacc
Matthias Guenther <matthias@wikimatze.de>
2017-05-03 23:26:31 +0200

    gromit: take a working version with not so many dependencies

--- scripts/gromit_mpx_install.sh
+++ scripts/gromit_mpx_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-cd /tmp && git clone https://github.com/bk138/gromit-mpx && cd gromit-mpx && autoreconf --install && ./configure && make && sudo make install
+VERSION=04bb86fa5bb82ee0443dbfe4fbef7a1739467df2
+cd /tmp && git clone https://github.com/bk138/gromit-mpx && cd gromit-mpx && git checkout $VERSION
+autoreconf --install && ./configure && make && sudo make install
 
