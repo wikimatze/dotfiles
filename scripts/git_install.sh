84d7c84c8a10064a4e95d0d35cc3d6f8781bb4d3
Matthias Guenther <matthias@wikimatze.de>
2017-05-03 23:09:17 +0200

    Update git to 2.12.2

--- scripts/git_install.sh
+++ scripts/git_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-VERSION=2.10.1
+VERSION=2.12.2
 sudo apt-get install asciidoc xmlto docbook2x -y
 cd /tmp && wget https://github.com/git/git/archive/v$VERSION.tar.gz && tar -zxf v$VERSION.tar.gz
 cd /tmp/git-$VERSION && make configure && ./configure --prefix=/usr && make all && sudo make install
+
