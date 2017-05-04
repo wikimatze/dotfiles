5a9c07e895010364e21b91a3eedfb7e4f348b57c
Matthias Guenther <matthias@wikimatze.de>
2017-05-04 17:21:03 +0200

    neovim: dump 0.2, and check for Ruby-Installation and make the update gem if it's needed

--- scripts/nvim_install_linux.sh
+++ scripts/nvim_install_linux.sh
@@ -1,7 +1,22 @@
 echo "Install all dependencies for neovim compilation"
-sudo apt-get install libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+sudo apt-get install libtool autoconf automake cmake g++ pkg-config unzip
 
-cd /tmp && git clone https://github.com/neovim/neovim.git && cd neovim && git checkout 77a4f8f235901778d889aed7c708e1608eac25f6
+cd /tmp && git clone https://github.com/neovim/neovim.git && cd neovim && git checkout 13ec521
 
-make && sudo make install
+make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
+
+which ruby > /dev/null 2>&1
+if [ $? = 1 ]; then
+  echo "Ruby is not installed"
+  exit 0;
+fi
+
+NEOVIM=$(gem list | grep neovim)
+if [ "$NEOVIM" ]; then
+  echo "neovim is installed, it will be updated"
+  gem update neovim
+else
+  echo "Install neovim gem"
+  gem install neovim
+fi
 
