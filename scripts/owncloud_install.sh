73595128124eeacf45f0b1a6093a39b1e000d256
Matthias Guenther <matthias@wikimatze.de>
2017-05-04 17:19:16 +0200

    Typo in owncloud install script

--- scripts/owncloud_install.sh
+++ scripts/owncloud_install.sh
@@ -3,7 +3,7 @@ wget http://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_16.04
 sudo apt-key add - < Release.key
 sudo apt-get update
 
-udo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/ /' > /etc/apt/sources.list.d/owncloud-client.list"
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/ /' > /etc/apt/sources.list.d/owncloud-client.list"
 sudo apt-get update
 sudo apt-get install -y owncloud-client
 
