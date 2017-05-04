7f13821d6ef257722090bb2229fbbe1c59158b9e
Matthias Guenther <matthias@wikimatze.de>
2017-05-04 21:39:00 +0200

    ttygif dump 1.4.0 version

--- scripts/ttygif_install.sh
+++ scripts/ttygif_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+VERSION=1.4.0
 sudo apt-get install -y imagemagick ttyrec gcc
-cd /tmp && git clone https://github.com/icholy/ttygif.git && cd ttygif && make && sudo cp ttygif /usr/local/bin
+cd /tmp && git clone https://github.com/icholy/ttygif.git && cd ttygif && git checkout $VERSION
+
+make && sudo cp ttygif ~/bin
 
