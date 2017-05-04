f1cc97097ad7fb79182fa61f3e5cdf932d7d9e3c
Matthias Guenther <matthias@wikimatze.de>
2017-05-04 06:17:16 +0200

    node: dump v7.10.0

--- scripts/node_npm_install.sh
+++ scripts/node_npm_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/nodejs/node.git && cd node && git checkout v6.5.0 && ./configure && make && sudo make install
-sudo npm install -g csslint jshint js-yaml claymate bower phantomjs-prebuilt gitbook-cli diff-so-fancy karma karma-cli karma-chrome-launcher karma-ng-html2js-preprocessor jasmine-core
+cd /tmp && git clone https://github.com/nodejs/node.git && cd node && git checkout v7.10.0 && ./configure && make && sudo make install
+sudo npm install -g csslint jshint js-yaml claymate bower phantomjs-prebuilt
+sudo npm install -g  gitbook-cli diff-so-fancy karma karma-cli karma-chrome-launcher karma-ng-html2js-preprocessor jasmine-core
 rm -rf /tmp/node
 
