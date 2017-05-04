1aa438116444153bb5f4c7fa3e10831e977d0015
Matthias Guenther <matthias@wikimatze.de>
2017-05-04 21:43:44 +0200

    vagrant dump 1.9.4

--- scripts/vagrant_install.sh
+++ scripts/vagrant_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+VERSION=1.9.4
 cd /tmp
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6_x86_64.deb
+  wget https://releases.hashicorp.com/vagrant/$VERSION/vagrant_"$VERSION"_x86_64.deb
 else
-
-  wget https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6_i686.deb
+  wget https://releases.hashicorp.com/vagrant/$VERSION/vagrant_"$VERSION"_i686.deb
 fi
 
 sudo dpkg -i vagrant*.deb
