e801b7f66c2ef00049c2dab572f6010fd1ed7889
Matthias Guenther <matthias@wikimatze.de>
2017-05-05 17:44:42 +0200

    Virtualbox dump 5.1.22

--- scripts/virtualbox_install.sh
+++ scripts/virtualbox_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+VERSION=5.1.22
+TIMESTAMP=115126
 cd /tmp
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget http://download.virtualbox.org/virtualbox/5.1.8/virtualbox-5.1_5.1.8-111374~Ubuntu~yakkety_amd64.deb
+  wget http://download.virtualbox.org/virtualbox/$VERSION/virtualbox-5.1_$VERSION-$TIMESTAMP~Ubuntu~trusty_amd64.deb
 else
-  wget http://download.virtualbox.org/virtualbox/5.1.8/virtualbox-5.1_5.1.8-111374~Ubuntu~yakkety_i386.deb
+  wget http://download.virtualbox.org/virtualbox/$VERSION/virtualbox-5.1_$VERSION-$TIMESTAMP~Ubuntu~trusty_i386.deb
 fi
 
 sudo dpkg -i virtualbox-*
