b9eb91004542522c7a295c0a74677eda19a07bd8
Matthias Guenther <matthias@wikimatze.de>
2017-05-05 18:07:13 +0200

    conky: sound view adjustments

--- conky/sound.sh
+++ conky/sound.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ `amixer| grep "'Master',0" -A 4 | grep -c "\[off\]"` -eq 1 ]; then
+if [ `amixer| grep "'Master',0" -A 10 | grep -c "\[off\]"` -ge 1 ]; then
   VERSION='𝄪'
 else
-  NUMBER="`amixer | grep "'Master',0" -A 4 | grep -o -m 1 -P '(\d{1,3}%)'`"
+  NUMBER="`amixer | grep "'Master',0" -A 10 | grep -o -m 1 -P '(\d{1,3}%)'`"
   VERSION='♬'
 fi
 
