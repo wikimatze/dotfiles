96c476d387d428ae15d606e2404594a93f58a424
Matthias Guenther <matthias@wikimatze.de>
2017-05-06 08:09:24 +0200

    Update zsh-install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,28 +2,71 @@ sudo bash ack_install.sh
 sudo bash ag_install.sh
 sudo bash bitbucket_repos_clone.sh
 sudo bash chromium_install.sh
+sudo bash cmus_install.sh
+sudo bash conky_install.sh
 sudo bash ctags_install.sh
-sudo bash fasd_install.sh
+sudo bash diff_so_fancy_install.sh
 
-sudo bash firefox_flash_update.sh
+sudo bash fasd_install.sh
+sudo bash ffmpeg_install.sh
+
+sudo bash git_install.sh
+sudo bash git_forked_repositories_checkout_install.sh
+sudo bash git_repositories_checkout_install.sh
+
+sudo bash gitter_install.sh
+sudo bash gromit_mpx_install.sh
 sudo bash grub_customizer.sh
+
+
 
 sudo bash handbrake_install.sh
 sudo bash heroku_install.sh
 sudo bash hub_install.sh
+
+sudo bash i3_install.sh
+
 sudo bash irssi_install.sh
+
+sudo bash latexmk_install.sh
+sudo bash libsynctex_install.sh
 sudo bash latex_packages_install.sh
-bash node_npm_install.sh
+
+sudo bash nvim_install_linux.sh
+
+sudo bash nextcloud_install.sh
+sudo bash node_npm_install.sh
+
+sudo bash pass_install.sh
+sudo bash pip_install.sh
 sudo bash pt_install.sh
-bash rvm_install.sh
-bash skype_install.sh
-bash tmuxifier_install.sh
-bash tmux_install.sh
-bash ttygif_install.sh
-bash vagrant_install.sh
-bash virtualbox_install.sh
-bash xfce4_terminal_install.sh
+sudo bash prezto_install.sh
 
+sudo bash ranger_install.sh
+sudo bash rofi_install.sh
+sudo bash rvm_install.sh
 
+sudo bash skype_install.sh
+sudo bash simplescreenrecorder_install.sh
+sudo bash symlink_install.sh
 
+sudo bash tig_install.sh
+sudo bash ttygif_install.sh
+
+sudo bash tmux_install.sh
+sudo bash tmux_plugins_install.sh
+
+sudo bash unetbootin_install.sh
+sudo bash vagrant_install.sh
+
+sudo bash vim_install_linux.sh
+
+sudo bash virtualbox_install.sh
+
+sudo bash weechat_install.sh
+sudo bash xfce4_terminal_install.sh
+
+sudo bash zathura_install.sh
+
+sudo bash zsh_install.sh
 
