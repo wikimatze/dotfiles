a98d8a8c770fac56f1420e2faa9d5ba1cc6c2dbf
Matthias Guenther <matthias@wikimatze.de>
2017-06-26 19:05:29 +0200

    Apply shellcheck for cmus script

--- conky/cmus.sh
+++ conky/cmus.sh
@@ -4,18 +4,16 @@ if [ ! -x /usr/local/bin/cmus-remote ]; then
   exit
 fi
 
-Cmus_remote=$(cmus-remote -Q)
-Instance=$(echo -e "$Cmus_remote" | wc -l)
-STREAM=`cmus-remote -Q | grep -c "stream "`
+INSTANCE=$(echo -e "$(cmus-remote -Q)" | wc -l)
+STREAM=$(cmus-remote -Q | grep -c "stream ")
 
-
-if [ $Instance -gt 1 ]; then
-  if [ $STREAM -eq 1 ]; then
-    STREAM=`cmus-remote -Q | grep "stream " | cut -c 8-`
-    echo $STREAM
+if [ "$INSTANCE" -gt 1 ]; then
+  if [ "$STREAM" -eq 1 ]; then
+    STREAM=$(cmus-remote -Q | grep "stream " | cut -c 8-)
+    echo "$STREAM"
   else
-    TITLE=`cmus-remote -Q | grep " title " | cut -c 11-`
-    ARTIST=`cmus-remote -Q | grep " albumartist " | cut -c 17-`
+    TITLE=$(cmus-remote -Q | grep " title " | cut -c 11-)
+    ARTIST=$(cmus-remote -Q | grep " albumartist " | cut -c 17-)
     echo "$TITLE" - "$ARTIST"
   fi
 fi
