96de2b924e243494468f9cc94a6d3cfd87626df4
Matthias Guenther <matthias@wikimatze.de>
2017-09-09 11:09:45 +0200

    Pimp gromit installation

--- scripts/gromit_mpx_install.sh
+++ scripts/gromit_mpx_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VERSION=04bb86fa5bb82ee0443dbfe4fbef7a1739467df2
-cd /tmp && git clone https://github.com/bk138/gromit-mpx && cd gromit-mpx && git checkout $VERSION
+cd /tmp && rm -rf gromit-mpx && git clone https://github.com/bk138/gromit-mpx && cd gromit-mpx && git checkout $VERSION
 autoreconf --install && ./configure && make && sudo make install
 
