e5b233f08933192f14d3b40dc7eb15cdd3246c9d
Matthias Guenther <matthias@wikimatze.de>
2017-09-09 10:46:32 +0200

    ffmpeg: easier installation

--- scripts/ffmpeg_install.sh
+++ scripts/ffmpeg_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 VERSION=n3.3
-cd /tmp && git clone https://github.com/FFmpeg/FFmpeg && cd FFmpeg && git checkout $VERSION && ./configure --disable-ffplay --disable-ffprobe --disable-ffserver --enable-libmp3lame --enable-libv4l2 --enable-libx264 --enable-libpulse --enable-gpl --enable-nonfree --disable-yasm  --extra-libs="-lasound" && make && sudo make install
+
+sudo apt-get build-dep -y ffmpeg
+
+cd /tmp && rm -rf FFmpeg && git clone https://github.com/FFmpeg/FFmpeg && cd FFmpeg && git checkout $VERSION && ./configure --disable-ffplay --disable-ffprobe --disable-ffserver --enable-libmp3lame --enable-libx264 --enable-libpulse --enable-gpl --enable-nonfree --disable-yasm  --extra-libs="-lasound" && make && sudo make install
 
