f744deca001ec1b9494ee912228121cfa4638f98
Matthias Guenther <matthias@wikimatze.de>
2017-09-09 18:43:32 +0200

    pip-install: do it for pip3 as well

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 pip install --upgrade pip
+pip3 install --upgrade pip
 
 sudo pip install docutils # need for previm
+sudo pip3 install docutils # need for previm
 
 # See the funny https://github.com/chrisallenlane/cheat
 sudo pip install cheat
+sudo pip3 install cheat
 
 sudo pip install neovim
+sudo pip3 install neovim
