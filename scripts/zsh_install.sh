637a1b8e931b5323766b66e702a3eacac32e0656
Matthias Guenther <matthias@wikimatze.de>
2017-09-16 08:45:03 +0200

    zsh: dump 5.4.2

--- scripts/zsh_install.sh
+++ scripts/zsh_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=zsh-5.3
+VERSION=zsh-5.4.2
 cd /tmp && git clone https://github.com/zsh-users/zsh.git && cd zsh && git checkout $VERSION
 ./Util/preconfig && ./configure && make && sudo make install
 
