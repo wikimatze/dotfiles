1452b2d05a95e38d4d4e1e8624239f21f71c4d14
Matthias Guenther <matthias@wikimatze.de>
2017-09-16 09:21:09 +0200

    diff-so-highlight: change installation paths

--- scripts/diff_so_fancy_install.sh
+++ scripts/diff_so_fancy_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIFF_HIGHLIGHT_VERSION=d2bbb7c
 cd /tmp && git clone https://github.com/git/git.git && cd git && git checkout $DIFF_HIGHLIGHT_VERSION
-sudo cp contrib/diff-highlight/diff-highlight ~/bin && sudo chmod +x ~/bin/diff-highlight
+sudo cp contrib/diff-highlight/diff-highlight /usr/local/bin && sudo chmod +x /usr/local/bin/diff-highlight
 
 
 DIFF_SO_FANCY_VERSION=3adf011
 cd /tmp && git clone https://github.com/so-fancy/diff-so-fancy.git && cd diff-so-fancy && git checkout $DIFF_SO_FANCY_VERSION
-sudo cp diff-so-fancy ~/bin && sudo chmod +x ~/bin/diff-so-fancy
+sudo cp diff-so-fancy /usr/local/bin && sudo chmod +x /usr/local/bin/diff-so-fancy
 
