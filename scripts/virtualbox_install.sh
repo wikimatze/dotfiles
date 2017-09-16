ef971a4ab233de834980d7d3ea021f68369fa86e
Matthias Guenther <matthias@wikimatze.de>
2017-09-16 10:20:44 +0200

    virtualbox: new version and add libvpx package

--- scripts/virtualbox_install.sh
+++ scripts/virtualbox_install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-VERSION=5.1.22
-TIMESTAMP=115126
+VERSION=5.1.28
+TIMESTAMP=117968
 cd /tmp
 if [ "$(uname -m)" == "x86_64" ]
 then
+  wget http://security.ubuntu.com/ubuntu/pool/main/libv/libvpx/libvpx1_1.0.0-1_amd64.deb
   wget http://download.virtualbox.org/virtualbox/$VERSION/virtualbox-5.1_$VERSION-$TIMESTAMP~Ubuntu~trusty_amd64.deb
 else
+  wget http://security.ubuntu.com/ubuntu/pool/main/libv/libvpx/libvpx1_1.0.0-1_i386.deb
   wget http://download.virtualbox.org/virtualbox/$VERSION/virtualbox-5.1_$VERSION-$TIMESTAMP~Ubuntu~trusty_i386.deb
 fi
 
+sudo dpkg -i libvpx*
 sudo dpkg -i virtualbox-*
 
