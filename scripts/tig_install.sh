63d21b79ba225e521f4779a8e491e5768bc0cfb8
Matthias Guenther <matthias@wikimatze.de>
2017-09-16 09:27:49 +0200

    tig: dump 2.2.2

--- scripts/tig_install.sh
+++ scripts/tig_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION=2.2.1
+VERSION=2.2.2
 cd /tmp && git clone https://github.com/jonas/tig.git && cd tig && git checkout tig-$VERSION
 sudo make prefix=/usr/local && sudo make install prefix=/usr/local
