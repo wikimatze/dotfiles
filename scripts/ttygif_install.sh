fd394b096d4bab6fa541c0ca2f2ee315f594ccb1
Matthias Guenther <matthias@wikimatze.de>
2017-09-16 09:36:43 +0200

    ttygif: change binary location

--- scripts/ttygif_install.sh
+++ scripts/ttygif_install.sh
@@ -3,5 +3,5 @@ VERSION=1.4.0
 sudo apt-get install -y imagemagick ttyrec gcc
 cd /tmp && git clone https://github.com/icholy/ttygif.git && cd ttygif && git checkout $VERSION
 
-make && sudo cp ttygif ~/bin
+make && sudo cp ttygif /usr/local/bin
 
