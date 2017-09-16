73895613471c0b1e32331471d5a4f8e47b4fe893
Matthias Guenther <matthias@wikimatze.de>
2017-09-16 09:51:55 +0200

    hub-install: improve script

--- scripts/hub_install.sh
+++ scripts/hub_install.sh
@@ -3,10 +3,10 @@ VERSION=2.2.9
 cd /tmp
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget https://github.com/github/hub/releases/download/v$VERSION/hub-linux-amd64-$VERSION.tgz
+  wget https://github.com/github/hub/releases/download/v$VERSION/hub-linux-amd64-$VERSION.tgz && 
   tar -xzvf hub-linux-amd64-$VERSION.tgz && sudo mv hub-linux-amd64-$VERSION/bin/hub /usr/local/bin
 else
-  wget https://github.com/github/hub/releases/download/v$VERSION/hub-linux-386-$VERSION.tgz
+  wget https://github.com/github/hub/releases/download/v$VERSION/hub-linux-386-$VERSION.tgz &&
   tar -xzvf hub-linux-386-$VERSION.tgz && sudo mv hub-linux-386-$VERSION/bin/hub /usr/local/bin
 fi
 
