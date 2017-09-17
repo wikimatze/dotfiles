4f2d12a2ca886054fe596e3bd9aba154db7d8223
Matthias Guenther <matthias@wikimatze.de>
2017-09-17 17:03:21 +0200

    conky: backlight script notice how to make file writable

--- conky/bright_down.sh
+++ conky/bright_down.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# make /sys/class/backlight/intel_backlight/brightness writable
+# /etc/rc.local => sudo chmod 777 /sys/class/backlight/intel_backlight/brightness
 brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
 
 if [ "$brightness" -gt "0" ]; then
--- conky/bright_up.sh
+++ conky/bright_up.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# make /sys/class/backlight/intel_backlight/brightness writable
+# /etc/rc.local => sudo chmod 777 /sys/class/backlight/intel_backlight/brightness
 max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
 brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
 
