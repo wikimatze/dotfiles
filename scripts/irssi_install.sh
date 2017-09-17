424e90b9da4c5747933a254169be6ec7aaa67f91
Matthias Guenther <matthias@wikimatze.de>
2017-09-17 16:03:45 +0200

    irssi: dump 1.0.4

--- scripts/irssi_install.sh
+++ scripts/irssi_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.0.2
+VERSION=1.0.4
 DIR=irssi-$VERSION
 
 cd /tmp && wget https://github.com/irssi/irssi/releases/download/$VERSION/$DIR.tar.gz && tar xf $DIR.tar.gz && cd $DIR && ./configure && make && sudo make install
