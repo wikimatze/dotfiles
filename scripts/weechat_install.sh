040545014790eb4fe4f5c5de524517fd982d7e53
Matthias Guenther <matthias@wikimatze.de>
2017-09-17 08:40:35 +0200

    weechat: dump 1.7 and a lot of config changes

--- scripts/weechat_install.sh
+++ scripts/weechat_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.6
+VERSION=1.7
 
 cd /tmp && rm -rf weechat && git clone https://github.com/weechat/weechat.git && cd weechat && git checkout v$VERSION && mkdir build && cd build && cmake .. && make && sudo make install
 
