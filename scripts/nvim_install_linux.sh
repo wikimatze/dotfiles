22047fa2bdbd927dde92c352ac71d99386bcb6d1
Matthias Guenther <matthias@wikimatze.de>
2017-09-17 16:06:53 +0200

    nvim: add more stable old version

--- scripts/nvim_install_linux.sh
+++ scripts/nvim_install_linux.sh
@@ -1,7 +1,7 @@
 echo "Install all dependencies for neovim compilation"
 sudo apt-get install libtool autoconf automake cmake g++ pkg-config unzip
 
-cd /tmp && git clone https://github.com/neovim/neovim.git && cd neovim && git checkout 13ec521
+cd /tmp && git clone https://github.com/neovim/neovim.git && cd neovim && git checkout 77a4f8f2
 
 make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install
 
