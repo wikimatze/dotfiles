673214d006dcad349ed4e6567a76f16800ff119d
Matthias Guenther <matthias@wikimatze.de>
2017-09-19 06:44:10 +0200

    Update install.sh and commented problematic files

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,18 @@
 sudo bash ack_install.sh
 sudo bash ag_install.sh
+
 sudo bash bitbucket_repos_clone.sh
+
 sudo bash chromium_install.sh
 sudo bash cmus_install.sh
 sudo bash conky_install.sh
 sudo bash ctags_install.sh
+
 sudo bash diff_so_fancy_install.sh
 
 sudo bash fasd_install.sh
-sudo bash ffmpeg_install.sh
+# sudo bash ffmpeg_install.sh (error, maybe not anymore ...)
 
-sudo bash git_install.sh
 sudo bash git_forked_repositories_checkout_install.sh
 sudo bash git_repositories_checkout_install.sh
 
@@ -18,24 +20,19 @@ sudo bash gitter_install.sh
 sudo bash gromit_mpx_install.sh
 sudo bash grub_customizer.sh
 
-
-
-sudo bash handbrake_install.sh
 sudo bash heroku_install.sh
 sudo bash hub_install.sh
 
 sudo bash i3_install.sh
 
-sudo bash irssi_install.sh
+# sudo bash irssi_install.sh
 
 sudo bash latexmk_install.sh
-sudo bash libsynctex_install.sh
-sudo bash latex_packages_install.sh
+# sudo bash latex_packages_install.sh (errors)
 
-sudo bash nvim_install_linux.sh
+# sudo bash nvim_install_linux.sh
 
-sudo bash nextcloud_install.sh
-sudo bash node_npm_install.sh
+# sudo bash node_npm_install.sh (extra)
 
 sudo bash pass_install.sh
 sudo bash pip_install.sh
@@ -63,9 +60,9 @@ sudo bash vim_install_linux.sh
 
 sudo bash virtualbox_install.sh
 
-sudo bash weechat_install.sh
 sudo bash xfce4_terminal_install.sh
 
+#sudo bash libsynctex_install.sh #(problems with texlive, had to remove the packages)
 sudo bash zathura_install.sh
 
 sudo bash zsh_install.sh
