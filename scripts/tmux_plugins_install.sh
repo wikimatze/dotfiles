27d31311cb893dc8203f7cc4f44ec96ddb190adf
Matthias Guenther <matthias@wikimatze.de>
2017-09-27 07:18:03 +0200

    tmux-plugin install: add plugins and urlview package

--- scripts/tmux_plugins_install.sh
+++ scripts/tmux_plugins_install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+git clone https://github.com/tmux-plugins/tmux-sensible ~/.tmux/plugins/tmux-sensible
+git clone https://github.com/tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
+git clone https://github.com/tmux-plugins/tmux-urlview ~/.tmux/plugins/tmux-urlview
+git clone https://github.com/tmux-plugins/tmux-battery ~/.tmux/plugins/tmux-battery
+
+# nedd for tmux-urlview
+sudo apt-get install urlview
+
