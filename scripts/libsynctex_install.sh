404e73a428672a240185ed676cdfa502cd111565
Matthias Guenther <matthias@wikimatze.de>
2017-10-06 08:07:23 +0200

    libsynctex: going from 2016 to 2015 version because of texlive packages

--- scripts/libsynctex_install.sh
+++ scripts/libsynctex_install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 cd /tmp && rm -rf libsynctex*
 
+# texlive-binaries : Depends: libsynctex1 (< 2015.20160222.37495-1.1~) but 2016.20160513.41080-6 is installed
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex1_2016.20160513.41080-6_amd64.deb
-  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex-dev_2016.20160513.41080-6_amd64.deb
+  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex1_2015.20160222.37495-1_amd64.deb
+  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex-dev_2015.20160222.37495-1_amd64.deb
 else
-  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex1_2016.20160513.41080-6_i386.deb
-  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex-dev_2016.20160513.41080-6_i386.deb
+  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex1_2015.20160222.37495-1_i386.deb
+  wget http://de.archive.ubuntu.com/ubuntu/pool/main/t/texlive-bin/libsynctex-dev_2015.20160222.37495-1_i386.deb
 fi
 
 sudo dpkg -i libsynctex1* && sudo dpkg -i libsynctex-dev*
