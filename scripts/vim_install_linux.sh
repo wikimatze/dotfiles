0c48414aa45e9138a80f00fae66c028a6176b2eb
Matthias Guenther <matthias@wikimatze.de>
2017-10-08 14:55:53 +0200

    Pimp vim-install: add version

--- scripts/vim_install_linux.sh
+++ scripts/vim_install_linux.sh
@@ -1,7 +1,8 @@
+VERSION=v8.0.1176
 echo "Install all dependencies for vim compilation"
 sudo apt-get build-dep vim -y
 
-cd /tmp && rm -rf vim && git clone https://github.com/vim/vim && cd vim && git checkout v8.0.0692 && git clean -f
+cd /tmp && rm -rf vim && git clone https://github.com/vim/vim && cd vim && git checkout $VERSION && git clean -f
 
 echo "Install xclip for better copy/paste"
 sudo apt-get install xclip
@@ -21,4 +22,6 @@ sudo apt-get install xclip
   --enable-fail-if-missing \
 
 sudo make && sudo make install && sudo make clean
+
+echo "In case you have problems with ruby, try rvm use system"
 
