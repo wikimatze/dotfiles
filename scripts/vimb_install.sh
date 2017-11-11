1b3cf367337af827852a589162b021c35242c753
Matthias Guenther <matthias@wikimatze.de>
2017-11-11 07:42:49 +0100

    Add vimb 3.0 alpha

--- scripts/vimb_install.sh
+++ scripts/vimb_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VERSION=2.12
-sudo apt-get install libsoup2.4-dev libwebkit-dev libgtk-3-dev libwebkitgtk-3.0-dev -y
+VERSION=3.0-alpha
+sudo apt-get install webkit2gtk-4.0 libsoup2.4-dev libwebkit-dev libgtk-3-dev libwebkitgtk-3.0-dev -y
 
 cd /tmp && git clone https://github.com/fanglingsu/vimb.git && cd vimb && git checkout $VERSION
 make && sudo make install && make clean
