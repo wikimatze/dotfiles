6ea15fe04a4bc64a0c9c1ec1ab43b4401387f85e
Matthias Guenther <matthias@wikimatze.de>
2017-11-21 20:08:46 +0100

    git: dump 2.15.0

--- scripts/git_install.sh
+++ scripts/git_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.14.1
+VERSION=2.15.0
 sudo apt-get install asciidoc xmlto docbook2x -y
 cd /tmp && wget https://github.com/git/git/archive/v$VERSION.tar.gz && tar -zxf v$VERSION.tar.gz
 cd /tmp/git-$VERSION && make configure && ./configure --prefix=/usr && make all && sudo make install
