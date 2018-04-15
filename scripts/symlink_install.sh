b6a51f39422e3fa685b1e85ed2e65b52efc919bf
Matthias Guenther <matthias@wikimatze.de>
2018-04-15 11:54:34 +0200

    symlinks script: using unlink function for removing symlinks

--- scripts/symlink_install.sh
+++ scripts/symlink_install.sh
@@ -4,100 +4,98 @@
 DIR=$HOME/nextcloud
 DOTFILES=$HOME/git/dotfiles
 
-rm -f $HOME/latex
-rm -f $HOME/.fonts
-rm -f $HOME/.zsh
-rm -f -R $HOME/.config/Ranger
-rm -f -R $HOME/.config/roxterm.sourceforge.net
-rm -f -R $HOME/.config/Terminal
-rm -rf $HOME/.i3
-rm -rf $HOME/.conky
-rm -f $HOME/.weechat
+unlink $HOME/latex
+unlink $HOME/.fonts
+unlink $HOME/.zsh
+unlink $HOME/.config/ranger
+unlink $HOME/.config/roxterm.sourceforge.net
+unlink $HOME/.config/Terminal
+unlink $HOME/.i3
+unlink $HOME/.conky
+unlink $HOME/.weechat
 
 # }}}
 # Bash files {{{
 
-rm -f $HOME/.bash_profile
-rm -f $HOME/.bashrc
-rm -f $HOME/.convert
-rm -f $HOME/.functions
-rm -f $HOME/.latex_convert_aliases
-rm -f $HOME/.miscellaneous
-rm -f $HOME/.program_aliases_linux
-rm -f $HOME/.program_aliases_mac
+unlink $HOME/.bashrc
+unlink $HOME/.convert
+unlink $HOME/.functions
+unlink $HOME/.latex_convert_aliases
+unlink $HOME/.miscellaneous
+unlink $HOME/.program_aliases_linux
+unlink $HOME/.program_aliases_mac
 
 # }}}
 # Miscellaneous {{{
 
-rm -f $HOME/.Xmodmap
-rm -f $HOME/.ackrc
-rm -f $HOME/.fasd
-rm -f $HOME/.gemrc
-rm -f $HOME/.gitconfig
-rm -f $HOME/.gitignore
-rm -f $HOME/.gitk
-rm -f $HOME/.irbrc
-rm -f $HOME/.irssi
-rm -f $HOME/.profile
-rm -f $HOME/.ptignore
-rm -f $HOME/.tmux.conf
-rm -f $HOME/.zprezto/modules/prompt/functions/prompt_wikimatze_setup
-rm -f $HOME/.zpreztorc
-rm -f $HOME/.zsh_history
-rm -f $HOME/.zshrc
-rm -f $HOME/.latexmkrc
-rm -f $HOME/.conkyrc
-rm -f $HOME/.weechat_passphrase
-rm -rf $HOME/.config/gromit-mpx.cfg
-rm -rf $HOME/.config/cmus/rc
-rm -rf $HOME/.config/cmus/album.sh
-rm -f $HOME/.config/qutebrowser/autoconfig.yml
-sudo rm -f /etc/default/apport
+unlink $HOME/.Xmodmap
+unlink $HOME/.ackrc
+unlink $HOME/.fasd
+unlink $HOME/.gemrc
+unlink $HOME/.gitconfig
+unlink $HOME/.gitignore
+unlink $HOME/.gitk
+unlink $HOME/.irbrc
+unlink $HOME/.irssi
+unlink $HOME/.profile
+unlink $HOME/.ptignore
+unlink $HOME/.tmux.conf
+unlink $HOME/.zprezto/modules/prompt/functions/prompt_wikimatze_setup
+unlink $HOME/.zpreztorc
+unlink $HOME/.zsh_history
+unlink $HOME/.zshrc
+unlink $HOME/.latexmkrc
+unlink $HOME/.conkyrc
+unlink $HOME/.weechat_passphrase
+unlink $HOME/.config/gromit-mpx.cfg
+unlink $HOME/.config/cmus/rc
+unlink $HOME/.config/cmus/album.sh
+unlink $HOME/.config/qutebrowser/autoconfig.yml
+sudo unlink /etc/default/apport
 
 # }}}
 # Mucksrcs {{{
 
-rm -f $HOME/bitbucket/die-lernhelfer-admin/.mucksrc
-rm -f $HOME/bitbucket/die-lernhelfer-website/.mucksrc
-rm -f $HOME/bitbucket/presentations/.mucksrc
-rm -f $HOME/bitbucket/padrinocasts/.mucksrc
-rm -f $HOME/git/PadrinoBook/.mucksrc
-rm -f $HOME/git/wikimatze.de/.mucksrc
-rm -f $HOME/git/job-vacancy/.mucksrc
-rm -f $HOME/git/padrino-web/.mucksrc
-rm -f $HOME/git/padrinobook-website/.mucksrc
-rm -f $HOME/git/pumog/.mucksrc
-rm -f $HOME/git/sweetie/.mucksrc
-rm -f $HOME/git/vimberlin.de/.mucksrc
-rm -f $HOME/git/vimfest/.mucksrc
-rm -f $HOME/git/padrino-web/.mucksrc
-rm -f $HOME/nextcloud/latex/projekte/javascript/.mucksrc
-rm -f $HOME/nextcloud/latex/projekte/ruby/.mucksrc
-rm -f $HOME/nextcloud/latex/projekte/scrum/.mucksrc
-rm -f $HOME/nextcloud/latex/projekte/kanban/.mucksrc
-rm -f $HOME/nextcloud/latex/projekte/angular/.mucksrc
-rm -f $HOME/nextcloud/latex/vorlagen/cv/.mucksrc
+unlink $HOME/bitbucket/die-lernhelfer-admin/.mucksrc
+unlink $HOME/bitbucket/die-lernhelfer-website/.mucksrc
+unlink $HOME/bitbucket/padrinocasts/.mucksrc
+unlink $HOME/bitbucket/presentations/.mucksrc
+unlink $HOME/git/PadrinoBook/.mucksrc
+unlink $HOME/git/job-vacancy/.mucksrc
+unlink $HOME/git/padrino-web/.mucksrc
+unlink $HOME/git/padrinobook.com/.mucksrc
+unlink $HOME/git/pumog/.mucksrc
+unlink $HOME/git/sweetie/.mucksrc
+unlink $HOME/git/vimberlin.de/.mucksrc
+unlink $HOME/git/vimfest/.mucksrc
+unlink $HOME/git/wikimatze.de/.mucksrc
+unlink $HOME/nextcloud/latex/projekte/angular/.mucksrc
+unlink $HOME/nextcloud/latex/projekte/javascript/.mucksrc
+unlink $HOME/nextcloud/latex/projekte/kanban/.mucksrc
+unlink $HOME/nextcloud/latex/projekte/ruby/.mucksrc
+unlink $HOME/nextcloud/latex/projekte/scrum/.mucksrc
+unlink $HOME/nextcloud/latex/vorlagen/cv/.mucksrc
 
 # }}}
 # Bin files {{{
 
-sudo rm -f /usr/local/bin/battery
-sudo rm -f /usr/local/bin/cjpgpdf.sh
-sudo rm -f /usr/local/bin/cpngpdf.sh
-sudo rm -f /usr/local/bin/crushjpg.sh
-sudo rm -f /usr/local/bin/crushpng.sh
-sudo rm -f /usr/local/bin/giffnaming.sh
-sudo rm -f /usr/local/bin/jnaming.sh
-sudo rm -f /usr/local/bin/jsmall.sh
-sudo rm -f /usr/local/bin/pnaming.sh
-sudo rm -f /usr/local/bin/psmall.sh
-sudo rm -f /usr/local/bin/waterproof.sh
-sudo rm -f /usr/local/bin/ttyconcat
-sudo rm -f /usr/local/bin/ttd
-sudo rm -f /usr/local/bin/ttl
-sudo rm -f /usr/local/bin/tts
-sudo rm -f /usr/local/bin/ttscreencast
-sudo rm -f /usr/local/bin/zoom_xfce.sh
+sudo unlink /usr/local/bin/battery
+sudo unlink /usr/local/bin/cjpgpdf.sh
+sudo unlink /usr/local/bin/cpngpdf.sh
+sudo unlink /usr/local/bin/crushjpg.sh
+sudo unlink /usr/local/bin/crushpng.sh
+sudo unlink /usr/local/bin/giffnaming.sh
+sudo unlink /usr/local/bin/jnaming.sh
+sudo unlink /usr/local/bin/jsmall.sh
+sudo unlink /usr/local/bin/pnaming.sh
+sudo unlink /usr/local/bin/psmall.sh
+sudo unlink /usr/local/bin/waterproof.sh
+sudo unlink /usr/local/bin/ttyconcat
+sudo unlink /usr/local/bin/ttd
+sudo unlink /usr/local/bin/ttl
+sudo unlink /usr/local/bin/tts
+sudo unlink /usr/local/bin/ttscreencast
+sudo unlink /usr/local/bin/zoom_xfce.sh
 
 # }}}
 # }}}
@@ -175,7 +173,7 @@ ln -sf $HOME/git/dotfiles/mucksrcs/kanban_mucksrc $HOME/nextcloud/latex/projekte
 ln -sf $HOME/git/dotfiles/mucksrcs/padrino_web_mucksrc $HOME/git/padrino-web/.mucksrc
 ln -sf $HOME/git/dotfiles/mucksrcs/padrinobook_mucksrc $HOME/git/PadrinoBook/.mucksrc
 ln -sf $HOME/git/dotfiles/mucksrcs/padrinocasts_mucksrc $HOME/bitbucket/padrinocasts/.mucksrc
-ln -sf $HOME/git/dotfiles/mucksrcs/padrinobook_website_mucksrc $HOME/git/padrinobook.com/.mucksrc
+ln -sf $HOME/git/dotfiles/mucksrcs/padrinobook_com_mucksrc $HOME/git/padrinobook.com/.mucksrc
 ln -sf $HOME/git/dotfiles/mucksrcs/presentations_mucksrc $HOME/bitbucket/presentations/.mucksrc
 ln -sf $HOME/git/dotfiles/mucksrcs/pumog_mucksrc $HOME/git/pumog/.mucksrc
 ln -sf $HOME/git/dotfiles/mucksrcs/ruby_mucksrc $HOME/nextcloud/latex/projekte/ruby/.mucksrc
