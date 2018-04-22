f3ec6650c897ddb6f7d9d97724affd7adcfc7e1f
Matthias Guenther <matthias@wikimatze.de>
2018-04-22 16:36:19 +0200

    Add rofi-pass install script

---
+++ scripts/rofi_pass_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+VERSION=2.0.1
+cd /tmp && rm -rf password-store && git clone https://github.com/carnager/rofi-pass.git && cd rofi-pass && git checkout $VERSION && sudo mv rofi-pass /usr/bin && sudo chmod 777 /usr/bin/rofi-pass
+
