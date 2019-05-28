55da580901131b341ad342fc3dcab8b3e4850425
Matthias Guenther <matthias@wikimatze.de>
2019-05-28 20:07:11 +0200

    Update qutebrowser install script

--- scripts/qutebrowser_install.sh
+++ scripts/qutebrowser_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=1.3.3
 sudo pip install tox
-cd /tmp && git clone https://github.com/qutebrowser/qutebrowser.git && cd qutebrowser && git checkout v$VERSION && tox -e mkvenv-pypi
+cd ~ && git clone https://github.com/qutebrowser/qutebrowser.git && cd qutebrowser && git checkout v$VERSION && tox -e mkvenv-pypi
 sudo cp .venv/bin/qutebrowser /usr/local/bin
 
