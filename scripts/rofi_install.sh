26894cd6e0c4c144c02f07f3c7c69a5c3c0324bd
Matthias Guenther <matthias@wikimatze.de>
2019-06-22 06:57:17 +0200

    Cleanup rofi installation

--- scripts/rofi_install.sh
+++ scripts/rofi_install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd /tmp
+VERSION='0.15.11-1'
+cd /tmp && rm rofi*
 if [ "$(uname -m)" == "x86_64" ]
 then
-  wget https://launchpad.net/ubuntu/+archive/primary/+files/rofi_0.15.11-1_amd64.deb
+  wget https://launchpad.net/ubuntu/+archive/primary/+files/rofi_$VERSION\_amd64.deb
 else
-  wget https://launchpad.net/ubuntu/+archive/primary/+files/rofi_0.15.11-1_i386.deb
+  wget https://launchpad.net/ubuntu/+archive/primary/+files/rofi_$VERSION\_i386.deb
 fi
 
 sudo dpkg -i rofi*.deb
