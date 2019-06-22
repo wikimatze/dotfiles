2bf4c4bbc0b361a909203498cc9d3cd6a241ca52
Matthias Guenther <matthias@wikimatze.de>
2019-06-22 07:01:26 +0200

    Update pass to 1.7.3

--- scripts/pass_install.sh
+++ scripts/pass_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.7.1
+VERSION=1.7.3
 cd /tmp && rm -rf password-store && git clone https://git.zx2c4.com/password-store && cd password-store && git checkout $VERSION && sudo make install
 
 
