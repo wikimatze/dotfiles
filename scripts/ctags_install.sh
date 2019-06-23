a50c237cadaf492faffd66172c0a1a9363e95f5f
Matthias Guenther <matthias@wikimatze.de>
2019-06-23 08:22:31 +0200

    Update ctags to latest version

--- scripts/ctags_install.sh
+++ scripts/ctags_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION=f1de2a8
+VERSION=f06928e
 cd /tmp && git clone https://github.com/universal-ctags/ctags.git && cd ctags && git checkout $VERSION && ./autogen.sh && ./configure && make && sudo make install
 
