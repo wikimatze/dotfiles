3e547b33c8b22c83df19f00ca0991f968201bc15
Matthias Guenther <matthias@wikimatze.de>
2019-12-19 04:43:05 +0100

    Pimp pip_install script

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# get old pip
+# https://pip.pypa.io/en/stable/installing/
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+
+# get pip3
+sudo apt install python3-pip -y 
+
 sudo pip install --upgrade pip
 sudo pip3 install --upgrade pip
 
