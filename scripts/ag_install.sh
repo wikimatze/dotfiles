47cbed6fa30bc88a3abc1dade90db145333a9e71
Matthias Guenther <matthias@wikimatze.de>
2019-12-21 05:38:31 +0100

    for the silversearcher > just take the latest package

--- scripts/ag_install.sh
+++ scripts/ag_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/ggreer/the_silver_searcher ag && cd ag && git checkout 2.2.0 && ./build.sh && sudo make install
+sudo apt-get install silversearcher-ag
+# to get the latest version
+#cd /tmp && git clone https://github.com/ggreer/the_silver_searcher ag && cd ag && git checkout 2.2.0 && ./build.sh && sudo make install
 
