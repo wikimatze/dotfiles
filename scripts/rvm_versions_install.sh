4f6feb1e94d2ef2e567ee18bd8b453a650110710
Matthias Guenther <matthias@wikimatze.de>
2021-04-21 19:48:58 +0200

    Update rvm install script

--- scripts/rvm_versions_install.sh
+++ scripts/rvm_versions_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rvm install ruby-1.9.3
-rvm install ruby-2.3.5
 rvm install ruby-2.5.1
-rvm install ruby-2.6.0dev
+rvm install ruby-2.6.5
+rvm install ruby-2.7.2
 
-rvm --default 2.6.0-preview1
+rvm --default 2.7.2
 
