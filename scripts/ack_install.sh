9ce502775df283da1f5c781958a58d6fc71734a9
Matthias Guenther <matthias@wikimatze.de>
2021-08-09 21:11:39 +0200

    ack dump version 3.5.0

--- scripts/ack_install.sh
+++ scripts/ack_install.sh
@@ -5,5 +5,5 @@ if [ $? = 1 ]; then
   sudo apt-get install -y curl
 fi
 
-curl https://beyondgrep.com/ack-v3.0.0 > /tmp/ack && sudo mv /tmp/ack /usr/local/bin/ack && sudo chmod 777 /usr/local/bin/ack
+curl https://beyondgrep.com/ack-v3.5.0 > /tmp/ack && sudo mv /tmp/ack /usr/local/bin/ack && sudo chmod 777 /usr/local/bin/ack
 
