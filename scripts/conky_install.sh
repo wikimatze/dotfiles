9d892c1f5aecc7594f65a47d4acff543fdc25b2f
Matthias Guenther <matthias@wikimatze.de>
2021-08-14 06:41:45 +0200

    conky dump version 1.12.2

--- scripts/conky_install.sh
+++ scripts/conky_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VERSION=1.10.6
-sudo apt-get install libtolua-dev libtolua++5.1-dev libx11-dev libxft-dev libxdamage-dev libncurses5-dev libxinerama-dev cmake -y
+VERSION=1.12.2
+sudo apt-get install libtolua-dev libtolua++5.1-dev libx11-dev libxft-dev libxdamage-dev libncurses5-dev libxinerama-dev cmake libimlib2-dev -y
 
 cd /tmp && git clone https://github.com/brndnmtthws/conky.git && cd conky && git checkout v$VERSION
 mkdir build && cd build && cmake .. && make && sudo make install
