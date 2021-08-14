97da898c2aaa3efe3f8bfd2a3c35f70cfec4444b
Matthias Guenther <matthias@wikimatze.de>
2021-08-14 05:34:51 +0200

    Update ctags installation

--- scripts/ctags_install.sh
+++ scripts/ctags_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION=f06928e
+VERSION=p5.9.20210808.0
 cd /tmp && git clone https://github.com/universal-ctags/ctags.git && cd ctags && git checkout $VERSION && ./autogen.sh && ./configure && make && sudo make install
 
