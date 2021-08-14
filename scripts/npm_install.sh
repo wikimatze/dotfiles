472e9ac5e512d2b6921816430c51753c60bd0890
Matthias Guenther <matthias@wikimatze.de>
2021-08-14 04:28:42 +0200

    Only install npm packages

--- scripts/npm_install.sh
+++ scripts/npm_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd /tmp && git clone https://github.com/nodejs/node.git && cd node && git checkout v7.10.0 && ./configure && make && sudo make install
 sudo npm install -g csslint jshint js-yaml claymate bower phantomjs-prebuilt
 sudo npm install -g  gitbook-cli diff-so-fancy karma karma-cli karma-chrome-launcher karma-ng-html2js-preprocessor jasmine-core
 rm -rf /tmp/node
