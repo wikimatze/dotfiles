b32859dd6c70abb79fcaa79117e878333a7162ba
Matthias Guenther <matthias@wikimatze.de>
2021-08-14 05:53:02 +0200

    zsh install 5.8

--- scripts/zsh_install.sh
+++ scripts/zsh_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=zsh-5.4.2
+VERSION=zsh-5.8
 cd /tmp && git clone https://github.com/zsh-users/zsh.git && cd zsh && git checkout $VERSION
 ./Util/preconfig && ./configure && make && sudo make install
 
