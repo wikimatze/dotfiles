6bc1206edffab3957da8d690cf315f602a6a59c9
Matthias Guenther <matthias@wikimatze.de>
2021-08-15 14:18:30 +0200

    hub tool is already into archive

--- scripts/archive/hub_install.sh
+++ scripts/archive/hub_install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+VERSION=2.2.9
+cd /tmp
 if [ "$(uname -m)" == "x86_64" ]
-  VERSION=hub-linux-amd64-2.2.1
 then
-  VERSION=hub-linux-386-2.2.1
+  wget https://github.com/github/hub/releases/download/v$VERSION/hub-linux-amd64-$VERSION.tgz && 
+  tar -xzvf hub-linux-amd64-$VERSION.tgz && sudo mv hub-linux-amd64-$VERSION/bin/hub /usr/local/bin
+else
+  wget https://github.com/github/hub/releases/download/v$VERSION/hub-linux-386-$VERSION.tgz &&
+  tar -xzvf hub-linux-386-$VERSION.tgz && sudo mv hub-linux-386-$VERSION/bin/hub /usr/local/bin
 fi
-
-cd /tmp && wget https://github.com/github/hub/releases/download/v2.2.1/$VERSION.tar.gz
-tar -zxvf $VERSION.tar.gz && sudo mv $VERSION/hub /usr/local/bin
-rm -rf /tmp/hub*
 
