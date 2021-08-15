945dbfd51d780d1f63a73631c125ad25b818d8ce
Matthias Guenther <matthias@wikimatze.de>
2021-08-15 15:23:19 +0200

    rofi-pass has now also a dependency

--- scripts/rofi_pass_install.sh
+++ scripts/rofi_pass_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 VERSION=2.0.2
+
+sudo apt-get install xdotool
+
+
 cd /tmp && rm -rf password-store && git clone https://github.com/carnager/rofi-pass.git && cd rofi-pass && git checkout $VERSION && sudo mv rofi-pass /usr/bin && sudo chmod 777 /usr/bin/rofi-pass
 
