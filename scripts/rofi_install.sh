2e0f65c30b70539f8baa992d531caa8120991c88
Matthias Guenther <matthias@wikimatze.de>
2021-08-15 15:13:57 +0200

    rofi has now a new dependency

--- scripts/rofi_install.sh
+++ scripts/rofi_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 VERSION='1.5.4-1'
 cd /tmp && rm rofi*
+
+sudo apt-get install libxcb-ewmh2
+
 if [ "$(uname -m)" == "x86_64" ]
 then
   wget https://launchpad.net/ubuntu/+archive/primary/+files/rofi_$VERSION\_amd64.deb
