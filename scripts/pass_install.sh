b5305301c621094ed49b59b60bf4c67e1d05c914
Matthias Guenther <matthias@wikimatze.de>
2021-08-15 14:54:15 +0200

    pass > dump version 1.7.4

--- scripts/pass_install.sh
+++ scripts/pass_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.7.3
+VERSION=1.7.4
 cd /tmp && rm -rf password-store && git clone https://git.zx2c4.com/password-store && cd password-store && git checkout $VERSION && sudo make install
 
 
