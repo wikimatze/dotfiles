5f32fd31e4911bff2b5bc2e5121cb788abbff6c7
Matthias Guenther <matthias@wikimatze.de>
2021-08-15 14:13:43 +0200

    zimfv -> dump 1.5.0

--- scripts/zimfv_install.sh
+++ scripts/zimfv_install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-VERSION=f11bb60
+VERSION='v1.5.0'
 rm -rf ~/.zim
 git clone --recursive https://github.com/zimfw/zimfw.git ${ZDOTDIR:-${HOME}}/.zim  && cd ~/.zim && git checkout $VERSION
+
+
+echo "Get tool to install modules"
+wget -O ~/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+
+
+echo "Install modules ..."
+zsh ~/.zim/zimfw.zsh install
+
+echo "Enjoy the terminal experience"
 
