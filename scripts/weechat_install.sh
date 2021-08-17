9f404406b1c780454012ec64bd0c5335891402a9
Matthias Guenther <matthias@wikimatze.de>
2021-08-17 14:10:46 +0200

    weechat dump version 2.3

--- scripts/weechat_install.sh
+++ scripts/weechat_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-VERSION=1.9
+VERSION=2.3
+
+sudo apt-get install -y libgcrypt20-dev
 
 cd /tmp && rm -rf weechat && git clone https://github.com/weechat/weechat.git && cd weechat && git checkout v$VERSION && mkdir build && cd build && cmake .. && make && sudo make install
 
