a81a170bc20310a72479581e9351864cffea63b4
Matthias Guenther <matthias@wikimatze.de>
2021-08-17 13:27:24 +0200

    qutebrowser > easier to install via package manager

--- scripts/qutebrowser_install.sh
+++ scripts/qutebrowser_install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-VERSION=1.3.3
-sudo pip install tox
-cd ~ && git clone https://github.com/qutebrowser/qutebrowser.git && cd qutebrowser && git checkout v$VERSION && tox -e mkvenv-pypi
-sudo cp .venv/bin/qutebrowser /usr/local/bin
+
+sudo apt-get install qutebrowser
 
