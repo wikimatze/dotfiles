31d086994a7ad5f59324fa03651367cadd45a369
Matthias Guenther <matthias@wikimatze.de>
2021-08-17 13:45:45 +0200

    ttygif > dump version 1.5.0

--- scripts/ttygif_install.sh
+++ scripts/ttygif_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VERSION=1.4.0
-sudo apt-get install -y imagemagick ttyrec gcc
+VERSION=1.5.0
+sudo apt-get install -y imagemagick ttyrec gcc x11-apps
 cd /tmp && git clone https://github.com/icholy/ttygif.git && cd ttygif && git checkout $VERSION
 
 make && sudo cp ttygif /usr/local/bin
