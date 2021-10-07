fcf6b93996ca899eef2db78d953411a13e66f2f2
Matthias Guenther <matthias@wikimatze.de>
2021-10-07 06:40:53 +0200

    Battery status adjustments

--- conky/battery.sh
+++ conky/battery.sh
@@ -3,7 +3,7 @@ if [ `acpi | grep -c Charging` -eq 1 ]; then
   CHARGE='▲'
   NUMBER="`acpi | grep -P -o '(\d{1,2}%)'`"
   NUMBER="$NUMBER charging: `acpi | grep Charging | grep -o -P '(\d{2}:\d{2})'`"
-elif [ `acpi | grep -c 'Full, 0%'` ]; then
+elif [ `acpi | grep -c 'Full, 0%'` -eq 1 ]; then
   CHARGE="⚡"
   NUMBER="AC"
 elif [ `acpi | grep -c Discharging` -eq 1 ]; then
