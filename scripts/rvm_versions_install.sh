b3b7386006d9bac33cb12860784deafd1db55590
Matthias Guenther <matthias@wikimatze.de>
2021-10-19 04:44:34 +0200

    Pimp rvm version install script

--- scripts/rvm_versions_install.sh
+++ scripts/rvm_versions_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-rvm install ruby-2.5.1
-rvm install ruby-2.6.5
-rvm install ruby-2.7.2
+rvm install ruby-2.5.9
+rvm install ruby-2.6.7
+rvm install ruby-2.7.4
 rvm install ruby-3.0.2
 
 exec $SHELL
 
-rvm --default 2.7.2
+# needed for gnome-terminal
+/bin/bash --login
 
+rvm --default 2.7.4
