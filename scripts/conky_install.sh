2e9c77698f669e2a5710de6000aac9db91821904
Matthias Guenther <matthias@wikimatze.de>
2026-06-16 13:55:02 +0200

    conky: Go from 1.12 to 1.80 (so from 2021 to 2023). Hell yeah

--- scripts/conky_install.sh
+++ scripts/conky_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.12.2
+VERSION=1.18.0
 sudo apt-get install libtolua-dev libtolua++5.1-dev libx11-dev libxft-dev libxdamage-dev libncurses5-dev libxinerama-dev cmake libimlib2-dev -y
 
 cd /tmp && git clone https://github.com/brndnmtthws/conky.git && cd conky && git checkout v$VERSION
