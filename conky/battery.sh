67e7780349b5863591de52fb99d247cd84df06de
Matthias Guenther <matthias@wikimatze.de>
2026-07-25 08:09:17 +0200

    conky battery: fix charging number with awk

--- conky/battery.sh
+++ conky/battery.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ `acpi | grep -c Charging` -eq 1 ]; then
   CHARGE='▲'
-  NUMBER="`acpi | grep -P -o '(\d{1,2}%)'`"
+  NUMBER="`acpi | awk -F', ' '/Charging/ {print $2}'`"
   NUMBER="$NUMBER charging: `acpi | grep Charging | grep -o -P '(\d{2}:\d{2})'`"
-elif [ `acpi | grep -c 'Full, 0%'` -eq 1 ]; then
+elif [ `acpi | grep -c 'Full, 100%'` -eq 1 ]; then
   CHARGE="⚡"
   NUMBER="AC"
 elif [ `acpi | grep -c Discharging` -eq 1 ]; then
